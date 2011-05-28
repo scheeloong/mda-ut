6e2744d48f833da5ac30018117e112fd8639f271
stetorvs@gmail.com <stetorvs@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2011-05-28 22:06:12 +0000

    Setup project to use DE0-Nano's Cyclone IV device. Added timing constraint file for CLOCK_50, which will be used later. Design works with Quartus 11.0.

--- fpga/mda_top.v
+++ fpga/mda_top.v
@@ -4,9 +4,9 @@
  * SOPC module, which contains NIOS and its Avalon slaves.
  */
 
-module mda_top (input [7:0]SW, output [7:0]LEDR);
-  // instantiate SW to LEDR connection
-  proc_independent_switch_led proc_ind(SW, LEDR);
+module mda_top (input [3:0]SW, output [3:0]LED);
+  // instantiate Switch to LED connection
+  proc_independent_switch_led proc_ind(SW, LED);
 
   // instantiate SOPC module
 
