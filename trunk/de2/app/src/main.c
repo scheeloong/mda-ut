dfaa04c20435c96a4683c7127849aa80932f9235
harfouchem <harfouchem@62d9add7-2908-a477-2ee2-5dc9016e4318>
2011-06-06 04:16:18 +0000

    Ok it should work, but the processor now goes in an infinite exception loop.
    I'll try to fix this tomorrow
    
    Everything should compile if you just type "make" on the top level module
    given that you are in a nios2_command_shell
    
    make sure you follow the instructions on the wiki to get it all going

--- trunk/de2/app/src/main.c
+++ trunk/de2/app/src/main.c
@@ -1,6 +1,18 @@
+#include "alt_types.h"
+#include <stdio.h>
+#include <unistd.h>
+#include "system.h"
+#include "sys/alt_irq.h"
+#include "altera_avalon_pio_regs.h"
 
 int main(){
-	while(1);
+
+	printf("Hello world\n");
+	IOWR_ALTERA_AVALON_PIO_DATA(LEDG_BASE, 0xbe);
+
+	while(1){
+
+	}
 
 	return 0;
 }
