8a8d607ce8656c3f955182effd28dce08a62128f
harfouchem <harfouchem@62d9add7-2908-a477-2ee2-5dc9016e4318>
2011-06-06 15:50:54 +0000

    playing with the seven segment display.
    Got a bunch of letters from wikipedia.
    I haven't tested all the letters so no guarantees

--- trunk/de2/app/src/main.c
+++ trunk/de2/app/src/main.c
@@ -12,8 +12,16 @@
 #include "sys/alt_irq.h"
 #include "altera_avalon_pio_regs.h"
 
+#include "seven_seg.h"
+
 int main(){
 	alt_u32 sw;
+	alt_u32 hex03;
+
+	IOWR_ALTERA_AVALON_PIO_DATA(HEX47_BASE, str_to_seven_seg("mda "));
+	IOWR_ALTERA_AVALON_PIO_DATA(HEX03_BASE, str_to_seven_seg("uoft"));
+
+
 	while(1){
 		sw = IORD_ALTERA_AVALON_PIO_DATA(SW_BASE);
 		IOWR_ALTERA_AVALON_PIO_DATA(LEDR_BASE, sw);
