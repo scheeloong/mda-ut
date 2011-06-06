576aa6c77680229168ddd93da5f5dc14b666974b
harfouchem <harfouchem@62d9add7-2908-a477-2ee2-5dc9016e4318>
2011-06-06 14:51:07 +0000

    wow it works now,
    not only do you have to specify the directory, but you need to specify the elf file, it won't automatically take one

--- trunk/de2/app/src/main.c
+++ trunk/de2/app/src/main.c
@@ -1,3 +1,10 @@
+/*
+ * main.c
+ *
+ *  Created on: 2011-06-06
+ *      Author: mark
+ */
+
 #include "alt_types.h"
 #include <stdio.h>
 #include <unistd.h>
@@ -6,12 +13,10 @@
 #include "altera_avalon_pio_regs.h"
 
 int main(){
-
-	printf("Hello world\n");
-	IOWR_ALTERA_AVALON_PIO_DATA(LEDG_BASE, 0xbe);
-
+	alt_u32 sw;
 	while(1){
-
+		sw = IORD_ALTERA_AVALON_PIO_DATA(SW_BASE);
+		IOWR_ALTERA_AVALON_PIO_DATA(LEDR_BASE, sw);
 	}
 
 	return 0;
