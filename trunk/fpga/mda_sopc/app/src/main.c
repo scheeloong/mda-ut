c8410420bc1f9c3d4e6cbf348fc351d5b743cad7
stetorvs@gmail.com <stetorvs@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2011-06-11 13:27:56 +0000

    Added an include file and modified Makefile.

--- trunk/fpga/mda_sopc/app/src/main.c
+++ trunk/fpga/mda_sopc/app/src/main.c
@@ -1,9 +1,10 @@
+#include "main.h"
 #include "system.h"
 #include "sys/alt_stdio.h"
 
 int main() {
 	while (1) {
-		alt_putstr("Hello World!");
+		alt_putstr(MSG);
 		while (alt_getchar() != '\n')
 			;
 	}
