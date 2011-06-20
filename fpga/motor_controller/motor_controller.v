2765aa65bf15fec8c13cd3acdf00983905f35d72
stetorvs@gmail.com <stetorvs@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2011-06-20 01:48:25 +0000

    Decreased the PWM frequency to ~50KHz.

--- fpga/motor_controller/motor_controller.v
+++ fpga/motor_controller/motor_controller.v
@@ -1,13 +1,13 @@
 // This is the motor_controller for each H-bridge circuit
 // Direction and on/off can be specified, as well as duty cycle
-// Change the PWM frequency by modifying duty_counter TODO: make this easier to change
+// Change the PWM frequency by modifying duty_counter's length in bits (and the padding in the comparison)
 // There is also a dead time to ensure that the H-bridge doesn't toggle between two on states in 10 microseconds
 
 module motor_controller (input clk, input dir, input on, input [4:0] duty_cycle, output reg [3:0] out);
   parameter dead_time = 9'd500; // 500 cycles = 10 microseconds
 
   reg [3:0] out_reg;
-  reg [4:0] duty_counter = 5'b0;
+  reg [4:0] duty_counter = 10'd0;
   reg [8:0] dead_time_counter = dead_time;
   reg [1:0] prev_in = 2'b00;
 
@@ -24,7 +24,7 @@ module motor_controller (input clk, input dir, input on, input [4:0] duty_cycle,
     endcase
     prev_in <= {dir, on};
     duty_counter <= duty_counter + 4'd1;
-    out <= ((duty_counter < duty_cycle) && (dead_time_counter == dead_time)) ? out_reg : 4'd0;
+    out <= ((duty_counter < {5'd0, duty_cycle}) && (dead_time_counter == dead_time)) ? out_reg : 4'd0;
   end
 
 endmodule
