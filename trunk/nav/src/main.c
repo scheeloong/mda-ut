a5732a1c118b3149611207caa80da662d4ffe880
stetorvs@gmail.com <stetorvs@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2011-07-18 00:02:40 +0000

    Fixing bugs so that it works on the board.

--- trunk/nav/src/main.c
+++ trunk/nav/src/main.c
@@ -14,32 +14,30 @@
 #include "utils.h"
 
 #define NUM_MOTORS 6
-
-volatile int *motor_addr = (int *)MOTOR_CONTROLLER_0_BASE;
-int motor_word = 0;
+#define MOTOR_CONTROLLER_0_DUTY_CYCLE (MOTOR_CONTROLLER_0_BASE + 32)
 
 void do_command(char *st)
 {
   int i;
   if (strncmp(st, "f", 1) == 0) {
     for (i = 0; i < NUM_MOTORS; i++) {
-      IOWR(MOTOR_CONTROLLER_0_BASE, i*4, 0x3);
+      IOWR(MOTOR_CONTROLLER_0_BASE, i, 0x3);
     }
     alt_putstr("going forward\n");
   } else if (strncmp(st, "r", 1) == 0) {
     for (i = 0; i < NUM_MOTORS; i++) {
-      IOWR(MOTOR_CONTROLLER_0_BASE, i*4, 0x1);
+      IOWR(MOTOR_CONTROLLER_0_BASE, i, 0x1);
     }
     alt_putstr("going in reverse\n");
   } else if (strncmp(st, "stop", 4) == 0) {
     for (i = 0; i < NUM_MOTORS; i++) {
-      IOWR(MOTOR_CONTROLLER_0_BASE, i*4, 0x0);
+      IOWR(MOTOR_CONTROLLER_0_BASE, i, 0x0);
     }
     alt_putstr("stopping\n");
   } else if (strncmp(st, "sd ", 3) == 0) {
     int dc = read_hex(&st[3]);
     for (i = 0; i < NUM_MOTORS; i++) {
-      IOWR(MOTOR_CONTROLLER_0_BASE, (8+i)*4, dc);
+      IOWR(MOTOR_CONTROLLER_0_DUTY_CYCLE, i, dc);
     }
     alt_putstr("setting duty cycle\n");
   } else if (strncmp(st, "ga", 2) == 0) {
@@ -47,16 +45,16 @@ void do_command(char *st)
     get_accel(&accel_data);
     switch (st[2]) {
       case 'x':
-        alt_printf("%04x\n", accel_data.x);
+        alt_printf("%x\n", accel_data.x);
         break;
       case 'y':
-        alt_printf("%04x\n", accel_data.y);
+        alt_printf("%x\n", accel_data.y);
         break;
       case 'z':
-        alt_printf("%04x\n", accel_data.z);
+        alt_printf("%x\n", accel_data.z);
         break;
       case '\n':
-        alt_printf("%04x,%04x,%04x\n", accel_data.x, accel_data.y, accel_data.z);
+        alt_printf("%x,%x,%x\n", accel_data.x, accel_data.y, accel_data.z);
         break;
     }
   } else {
