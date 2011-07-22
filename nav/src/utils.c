f2e621e79e536453751afe6b76b10ba08dff8261
stetorvs@gmail.com <stetorvs@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2011-07-22 02:00:27 +0000

    Added a settings.h file that can enable settings, for example INTERACTIVE mode and logging.

--- nav/src/utils.c
+++ nav/src/utils.c
@@ -11,13 +11,16 @@
 #include "system.h"
 #include "sys/alt_stdio.h"
 
+#include "settings.h"
 #include "utils.h"
 
 void alt_getline(char *st, int len)
 {
   while (len--) {
     char c = (char)alt_getchar();
-	alt_putchar(c);
+    if (INTERACTIVE) {
+      alt_putchar(c);
+    }
     *st++ = c;
     if (c == '\n')
       break;
@@ -42,13 +45,13 @@ int read_hex(char *st)
 void get_accel(struct t_accel_data *accel_data)
 {
   IOWR(SELECT_I2C_CLK_BASE, 0, 0x00);
-    
+
   // configure accelerometer as +-2g and start measure
   if (!ADXL345_SPI_Init(GSENSOR_SPI_BASE)) {
     // could not configure accelerometer
     return;
   }
-    
+
   while (1) {
     if (ADXL345_SPI_IsDataReady(GSENSOR_SPI_BASE) && ADXL345_SPI_XYZ_Read(GSENSOR_SPI_BASE, accel_data)) {
       break;
