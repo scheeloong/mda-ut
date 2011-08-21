e2425d8be0165c86dfcd551da8513d5b1c01369e
stetorvs@gmail.com <stetorvs@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2011-08-21 22:04:21 +0000

    Implemented (and tested on board and simulation) anti-phase waveform to charge boost cap on motor circuit. This waveform drives in reverse when it is not driving forward.

--- fpga/motor_controller/motor_controller.v
+++ fpga/motor_controller/motor_controller.v
@@ -5,28 +5,40 @@
 
 `define MAX_DC (1024*`MAX_DUTY_CYCLE_FRAC)
 
-module motor_controller (input clk, input dir, input on, input [`DUTY_CYCLE_SIZE-1:0] duty_cycle, output reg [3:0] out);
+module motor_controller (input clk, input dir, input on, input [`DUTY_CYCLE_SIZE-1:0] duty_cycle, output [3:0] out);
+
+  reg [`FREQ_NEG_POW-1:0] duty_counter = 0;
+  reg dir_reg;
+
+  always @(posedge clk)
+  begin
+    duty_counter <= duty_counter + `DUTY_CYCLE_SIZE'd1;
+    dir_reg <= ((duty_counter[`FREQ_NEG_POW-1:`FREQ_NEG_POW-`DUTY_CYCLE_SIZE] < duty_cycle) && (duty_counter[`FREQ_NEG_POW-1:`FREQ_NEG_POW-`DUTY_CYCLE_SIZE] < `MAX_DC)) ? dir : ~dir;
+  end
+
+  motor_internal mi(clk, dir_reg, on, out);
+
+endmodule
+
+module motor_internal (input clk, input dir, input on, output reg [3:0] out);
 
   reg [3:0] out_reg;
-  reg [`FREQ_NEG_POW-1:0] duty_counter = 0;
-  reg [8:0] dead_time_counter = `DEAD_TIME;
+  reg [6:0] dead_time_counter = `DEAD_TIME;
   reg [1:0] prev_in = 2'b00;
 
   always @(posedge clk)
   begin
     if ({dir, on} != prev_in)
-      dead_time_counter <= 9'd0;
+      dead_time_counter <= 6'd0;
     if (dead_time_counter != `DEAD_TIME)
-      dead_time_counter <= dead_time_counter + 9'd1;
+      dead_time_counter <= dead_time_counter + 6'd1;
     casex ({dir, on})
       2'bx0: out_reg <= 4'b0000;
       2'b11: out_reg <= 4'b1001;
       2'b01: out_reg <= 4'b0110;
     endcase
     prev_in <= {dir, on};
-    duty_counter <= duty_counter + `DUTY_CYCLE_SIZE'd1;
-    out <= ((duty_counter[`FREQ_NEG_POW-1:`FREQ_NEG_POW-`DUTY_CYCLE_SIZE] < duty_cycle) && (duty_counter[`FREQ_NEG_POW-1:`FREQ_NEG_POW-`DUTY_CYCLE_SIZE] < `MAX_DC) && (dead_time_counter == `DEAD_TIME)) ? out_reg : 4'd0;
+    out <= (dead_time_counter == `DEAD_TIME) ? out_reg : 4'd0;
   end
 
 endmodule
-
