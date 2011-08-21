3b7e6da49189a7d3f9f6cb23adfbae2d1876a52d
stetorvs@gmail.com <stetorvs@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2011-08-21 00:50:07 +0000

    Improved modularity of Verilog code. Using GPIO pins, which were used to test the motor bridge circuit.

--- trunk/fpga/motor_controller/slave_controller.v
+++ trunk/fpga/motor_controller/slave_controller.v
@@ -3,9 +3,11 @@
 // Registers 8-13 store the respective duty cycle
 // The output should be fed to GPIO pins in the SOPC configuration
 
+`include "defines.v"
+
 module slave_controller(input clk, input chipselect, input write, input [3:0]addr, input [31:0] writedata, output [23:0] GPIO_out);
   reg [11:0] in;
-  reg [23:0] duty_cycle;
+  reg [6*`DUTY_CYCLE_SIZE-1:0] duty_cycle;
   
   always @(posedge clk)
     if (chipselect & write)
@@ -23,26 +25,26 @@ module slave_controller(input clk, input chipselect, input write, input [3:0]add
       4'b0101:
         in[11:10] <= writedata[1:0];
       4'b1000:
-        duty_cycle[3:0] <= writedata[3:0];
+        duty_cycle[`DUTY_CYCLE_SIZE-1:0] <= writedata[`DUTY_CYCLE_SIZE-1:0];
       4'b1001:
-        duty_cycle[7:4] <= writedata[3:0];
+        duty_cycle[2*`DUTY_CYCLE_SIZE-1:`DUTY_CYCLE_SIZE] <= writedata[`DUTY_CYCLE_SIZE-1:0];
       4'b1010:
-        duty_cycle[11:8] <= writedata[3:0];
+        duty_cycle[3*`DUTY_CYCLE_SIZE-1:2*`DUTY_CYCLE_SIZE] <= writedata[`DUTY_CYCLE_SIZE-1:0];
       4'b1011:
-        duty_cycle[15:12] <= writedata[3:0];
+        duty_cycle[4*`DUTY_CYCLE_SIZE-1:3*`DUTY_CYCLE_SIZE] <= writedata[`DUTY_CYCLE_SIZE-1:0];
       4'b1100:
-        duty_cycle[19:16] <= writedata[3:0];
+        duty_cycle[5*`DUTY_CYCLE_SIZE-1:4*`DUTY_CYCLE_SIZE] <= writedata[`DUTY_CYCLE_SIZE:0];
       4'b1101:
-        duty_cycle[23:20] <= writedata[3:0];
+        duty_cycle[6*`DUTY_CYCLE_SIZE-1:5*`DUTY_CYCLE_SIZE] <= writedata[`DUTY_CYCLE_SIZE:0];
       default:
         ; // do nothing
     endcase
 
   generate
   genvar i;
-    for (i=4; i<6; i=i+1)
+    for (i=3; i<6; i=i+1)
       begin : motor_control_loop
-        motor_controller mc(clk, in[i*2 + 1], in[i*2], duty_cycle[i*4+3:i*4], GPIO_out[i*4+3:i*4]);
+        motor_controller mc(clk, in[i*2 + 1], in[i*2], duty_cycle[(i+1)*`DUTY_CYCLE_SIZE-1:i*`DUTY_CYCLE_SIZE], GPIO_out[i*4+3:i*4]);
       end
   endgenerate
 endmodule
