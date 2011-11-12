c19db9857167ee28f151c3abea1b89d90bf5892f
stetorvs@gmail.com <stetorvs@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2011-11-12 05:11:53 +0000

    Changing GPIO for motor driver output and fixing help on NIOS code.

--- fpga/fpga_hw/top_level/DE0_Nano.v
+++ fpga/fpga_hw/top_level/DE0_Nano.v
@@ -179,7 +179,7 @@ DE0_Nano_SOPC DE0_Nano_SOPC_inst(
 
 			
                       // GPIO pins to Avalon slave(s)
-                       .GPIO_out_from_the_motor_controller_0 ({LED[7:0], GPIO_0[7], GPIO_0[5], GPIO_0[3], GPIO_0[1], GPIO_1[11:0]}),
+                       .GPIO_out_from_the_motor_controller_0 ({LED[7:0], GPIO_1[7], GPIO_1[5], GPIO_1[3], GPIO_1[1], GPIO_0[11:0]}),
 							  
                       // the_select_i2c_clk
                        .out_port_from_the_select_i2c_clk(select_i2c_clk),							  
