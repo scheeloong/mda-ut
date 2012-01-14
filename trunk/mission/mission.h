fca3d9702bdecb240c4732d29af85d6e820c0a47
stetorvs@gmail.com <stetorvs@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2012-01-14 23:01:55 +0000

    Fixing makefiles to account for changing .h files. Adding more commands to mission.h for the simulator.

--- trunk/mission/mission.h
+++ trunk/mission/mission.h
@@ -10,10 +10,21 @@ enum MOVE_DIR {
   RISE
 };
 
+enum DEBUG_CMD {
+  SCREENSHOT,
+  RESET_POS,
+  RESET_ALL,
+  EXIT
+};
+
 class Mission {
   public:
     void move(MOVE_DIR dir, int speed);
     void move(MOVE_DIR dir) { move(dir, DEFAULT_SPEED); }
+
+    // Only for the simulator
+    void translate(MOVE_DIR dir);
+    void debug(DEBUG_CMD cmd);
   private:
     int v_forward, v_depth, v_yaw;
 };
