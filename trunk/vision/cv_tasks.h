3af10775fdfde6b41e3472c85282827d340ba969
stetorvs@gmail.com <stetorvs@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2012-01-28 18:17:49 +0000

    Readying controller_GATE to use new Mission code to give commands to the sub.

--- trunk/vision/cv_tasks.h
+++ trunk/vision/cv_tasks.h
@@ -23,7 +23,7 @@ char vision_GATE (IplImage* img, int &gateX, int &gateY, float &range,
                   HSV_settings HSV, char* window[], char flags=0);
 char vision_SQUARE (IplImage* img, int &X, int &Y, float &range, 
                     HSV_settings HSV, char* window[], char flags=0);
-char controller_GATE (IplImage* img, char &state, char* window[]);
+void controller_GATE (IplImage* img, Mission &m);
 
 #define PATH_SKINNYNESS 0.12 // ratio of pipe diameter to length
 char vision_PATH (IplImage* img, int &pathX, int &pathY, float &tan_angle, float &length, 
--- trunk/vision/task_gate.h
+++ trunk/vision/task_gate.h
@@ -8,12 +8,14 @@
 #include "obj_detection.h"
 #include "calc_draw.h"
 
+#include "../mission/mission.h"
+
 #define GATE_SKINNYNESS 0.08 // ratio of pipe diameter to length
 #define GATE_WIDTH 300.0 // physical width in centimeters
 #define GATE_HEIGHT 160.0 // physical width in centimeters
 
 retcode vision_GATE (vision_in &Input, vision_out &Output, char flags=0);
 
-char controller_GATE (vision_in Input, char &state);
+void controller_GATE (vision_in Input, Mission &m);
 
 #endif
