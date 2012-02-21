41c7b232d8a483405f729ddfac7ed51200ceeef1
rzhao01@gmail.com <rzhao01@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2012-02-21 17:27:45 +0000

    added task_U and coded parts of it.Not finished.

---
+++ trunk/vision/task_U.h
@@ -0,0 +1,21 @@
+#ifndef _TASK_U
+#define _TASK_U
+
+#include <cv.h>
+#include <highgui.h>
+
+#include "preproc_filters.h"
+#include "obj_detection.h"
+#include "calc_draw.h"
+
+#include "../mission/mission.h"
+
+#define U_SKINNYNESS 0.08 // ratio of pipe diameter to length
+#define U_WIDTH 300.0 // physical width in centimeters
+#define U_HEIGHT 160.0 // physical width in centimeters
+
+retcode vision_U (vision_in &Input, vision_out &Output, char flags=0);
+
+void controller_U (vision_in &Input, Mission &m);
+
+#endif
