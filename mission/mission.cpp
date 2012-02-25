bc24f1b2b9f48028915034efbf270a267db11ae6
rzhao01@gmail.com <rzhao01@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2012-02-25 21:30:31 +0000

    fixed movement bugs changed path controller a bit fixed sim makefile

--- mission/mission.cpp
+++ mission/mission.cpp
@@ -3,7 +3,7 @@
 
 void Mission::move(MOVE_DIR dir, int speed)
 {
-   switch(dir) {
+   switch(dir) { // Ritchie: changed relative speeds
    case FORWARD:
       model->speed = speed;
       break;
@@ -11,16 +11,16 @@ void Mission::move(MOVE_DIR dir, int speed)
       model->speed = -speed;
       break;
    case RISE:
-      model->depth_speed = speed;
+      model->depth_speed = 3*speed;
       break;
    case SINK:
-      model->depth_speed = -speed;
+      model->depth_speed = -3*speed;
       break;
    case RIGHT:
-      model->angular_speed = speed;
+      model->angular_speed = 5*speed;
       break;
    case LEFT:
-      model->angular_speed = -speed;
+      model->angular_speed = -5*speed;
       break;
    case STOP:
       model->speed = 0;
@@ -34,16 +34,20 @@ void Mission::translate(MOVE_DIR dir)
 {
    switch(dir) {
    case FORWARD:
-      model->position.z -= POS_INC*cos((model->angle.yaw*PI)/180)*cos(((model->angle.pitch)*PI)/180);
+      model->position.z -= POS_INC*cos((model->angle.yaw*PI)/180)*cos((model->angle.pitch*PI)/180);
+      model->position.x += POS_INC*sin((model->angle.yaw*PI)/180)*cos((model->angle.pitch*PI)/180);
+      model->position.y -= POS_INC*sin((model->angle.pitch*PI)/180);
       break;
    case REVERSE:
-      model->position.z += POS_INC*cos((model->angle.yaw*PI)/180)*cos(((model->angle.pitch)*PI)/180);
+      model->position.z += POS_INC*cos((model->angle.yaw*PI)/180)*cos((model->angle.pitch*PI)/180);
+      model->position.x -= POS_INC*sin((model->angle.yaw*PI)/180)*cos((model->angle.pitch*PI)/180);
+      model->position.y += POS_INC*sin((model->angle.pitch*PI)/180);
       break;
    case RISE:
       model->position.y += POS_INC;
       break;
    case SINK:
-      model->position.y += POS_INC;
+      model->position.y -= POS_INC;
       break;
    case RIGHT:
       model->angle.yaw += ANGLE_INC/2;
