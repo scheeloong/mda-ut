e3b65cac114b97cba09410520d6aa117f3683d65
stetorvs@gmail.com <stetorvs@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2012-03-17 01:07:40 +0000

    Implementing power management on the FPGA (not tested).

---
+++ trunk/fpga/fpga_hw/top_level/power_management/global_disable.v
@@ -0,0 +1,23 @@
+// Global disable
+//
+// If any of the shutdown inputs goes high, all GPIO outputs will be set to 0
+
+module global_disable #(
+  parameter NUM_IN = 1,
+  parameter NUM_IOS = 1
+) (
+  input clk,
+  input [NUM_IN-1:0] shutdown,
+  input [NUM_IOS-1:0] gpio_in,
+  output reg [NUM_IOS-1:0] gpio_out
+);
+
+  always @(posedge clk)
+    if (|shutdown)
+      // Set all GPIO outputs to low
+      gpio_out = {NUM_IOS{1'b0}};
+    else
+      // Set all GPIO outputs to the input
+      gpio_out = gpio_in;
+
+endmodule
