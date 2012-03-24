75e60d21690feb660861634af151c595d598f6d4
stetorvs@gmail.com <stetorvs@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2012-03-24 03:59:30 +0000

    Enable power management test project to cycle through mux configurations.

--- trunk/fpga/fpga_hw/demo_projects/power_management/power_management.v
+++ trunk/fpga/fpga_hw/demo_projects/power_management/power_management.v
@@ -1,13 +1,20 @@
-module power_management(input [3:0]SW, output [3:0]LED, inout [33:0]GPIO_1);
+// If all switches are on, then rotate through 0-7 for the output.
+// Otherwise, the output is controlled by the switches.
+module power_management(input CLOCK_50, input [3:0]SW, output [3:0]LED, inout [33:0]GPIO_1);
   wire [2:0] voltage_mux;
+  reg [28:0] counter = 29'd0;
 
   // kill switch
   assign GPIO_1[33] = SW[0];
+
   // voltage muxes
   assign {GPIO_1[29], GPIO_1[31], GPIO_1[25]} = voltage_mux;
-  assign voltage_mux = SW[3:1];
+  assign voltage_mux = (&SW[3:1]) ? counter[28:26] : SW[3:1];
 
   // LEDs
   assign LED[3:1] = voltage_mux;
   assign LED[0] = GPIO_1[27];
+
+  always @(posedge CLOCK_50)
+    counter = counter + 29'd1;
 endmodule
