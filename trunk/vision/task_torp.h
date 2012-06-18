493fc6ec80ba01fd44447cc23d657870dc3e3a90
vincentlu@hotmail.ca <vincentlu@hotmail.ca@62d9add7-2908-a477-2ee2-5dc9016e4318>
2012-06-18 23:04:16 +0000

    control code for torp working (basically) on sim

--- trunk/vision/task_torp.h
+++ trunk/vision/task_torp.h
@@ -7,10 +7,10 @@
 #include "preproc_filters.h"
 #include "obj_detection.h"
 #include "calc_draw.h"
-
-//#define BUOY_REAL_RAD 12.0 // in centimeters
+#include "../motors/motors.h"
+#define BUOY_REAL_RAD 12.0 // in centimeters
 
 retcode vision_TORP (vision_in &Input, vision_out &Output, char flags=0);
-char controller_TORP (vision_in Input, char &state);
+void controller_TORP (vision_in &Input, Motors &m);
 
 #endif
