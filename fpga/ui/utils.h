3925e4647ff2e68449534050460dfde8e665c283
rzhao01@gmail.com <rzhao01@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2012-07-01 03:50:05 +0000

    Finished fpga ui code, netbook side. fpga side not started.

---
+++ fpga/ui/utils.h
@@ -0,0 +1,41 @@
+#ifndef __MDA_UI_UTILS__
+#define __MDA_UI_UTILS__
+
+#define PWM_MAX 100
+
+#define H_FRONT_LEFT 0x1 // in hex
+#define H_FRONT_RIGHT 0x2
+#define H_FWD_LEFT 0x4
+#define H_FWD_RIGHT 0x8
+#define H_REAR 0x10
+#define M_FRONT_LEFT 1 // in decimal
+#define M_FRONT_RIGHT 2
+#define M_FWD_LEFT 3
+#define M_FWD_RIGHT 4
+#define M_REAR 5
+
+#define H_FWD 0xc
+#define H_RISE 0x13
+#define H_ALL 0x1f
+
+extern unsigned cmd_ok;
+
+int atoi_safe (char *str);
+void exit_safe ();
+void cmd_error ();
+
+void help ();
+void help_motor ();
+void help_dyn ();
+void help_power ();
+
+void motor_status();
+void motor_set (int pwm, char motor_flags);
+
+void power_status();
+void power_on();
+void power_off();
+
+void dyn_status();
+
+#endif
