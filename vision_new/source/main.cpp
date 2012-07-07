f59cba8a1396ac13b77b37e7c1ff198baa46db45
rzhao01@gmail.com <rzhao01@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2012-07-07 04:52:14 +0000

    Added mvWindow class. Add makefile INC dependancy

--- vision_new/source/main.cpp
+++ vision_new/source/main.cpp
@@ -13,8 +13,14 @@ int main () {
     
     read_mv_setting (filename, "S1", S1);
     read_mv_setting (filename, "S2", S2);
+    //read_mv_setting (filename, "S3", S2);
     
     printf ("S1: %f\nS2: %f\n", S1, S2);
                       
+    mvWindow win1("win1");
+    mvWindow win2("win2");
+    mvWindow win3("win3");
+    mvWindow win4("win4");
+    cvWaitKey(0);
     return 0;
 }
