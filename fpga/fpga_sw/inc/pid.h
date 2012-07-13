3f706b3c851806f4ebedd3e5cb4ed3b0e3af3b43
rzhao01@gmail.com <rzhao01@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2012-07-13 04:46:22 +0000

    Added PID controller and motor code to fpga. Have not compiled. Because I was confused by the existing code, I added the code in the form of calculate_pid_2, which is an alternative to calculate_pid which can be used by #defining USE_PID_2 in settings.h All the code was put into controller.c and pid.c

---
+++ fpga/fpga_sw/inc/pid.h
@@ -0,0 +1,24 @@
+// controller_new.h - temporary header for embedded control code
+#ifndef _PID_H
+#define _PID_H
+
+#include "controller_settings.h"
+
+#define PID_NUM_OLD_VALUES 3
+
+typedef struct {
+    // everything is here is supposed to be private
+    // but for setting up the constants we'll access the members directly
+    HW_NUM P, I, D;
+    HW_NUM Const_P, Const_I, Const_D;
+    float Alpha; // decay constant for integral term
+    
+    int num_values; // number of values read
+    HW_NUM old_values[PID_NUM_OLD_VALUES];
+} Controller_PID;
+
+void PID_Reset (Controller_PID* PID);
+void PID_Update (Controller_PID* PID, HW_NUM value);
+HW_NUM PID_Output (Controller_PID* PID);
+
+#endif
