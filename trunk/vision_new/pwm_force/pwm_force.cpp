bf1434518fc115763201bd4da6e28939da26d5ab
vincentlu@hotmail.ca <vincentlu@hotmail.ca@62d9add7-2908-a477-2ee2-5dc9016e4318>
2012-07-25 06:25:53 +0000

    fixed some things for the motor linearization code. added proper equations to get the proper pwms (note: pwms are ints, and so the back motor pwm values add up to 1 less than the pwm of the front motor). printed the lookup (strangely they're all 1's)

--- trunk/vision_new/pwm_force/pwm_force.cpp
+++ trunk/vision_new/pwm_force/pwm_force.cpp
@@ -3,76 +3,94 @@
 #include <stdlib.h>
 // given a force, this function returns the pwm to generate that force using the equations above
 // or the lookup, if necessary.
-double pwm_force_pos (double force) {
-  int positive_pwm = pwm_greater_than (force);
+
+// declare this array so it can be used on initializing the look-up
+int pwm_of_force[SIZE];
+
+int pwm_greater_than(double force) {
+  return (int)((force + 0.698636363)/0.045);
+}
+
+int pwm_lesser_than(double force) {
+  return (int)((force - 0.82990909)/0.0502);
+}
+
+double force_cubic(int pwm) {
+  return (-5e-7)*pow(pwm, 3) + 0.0005*pow(pwm,2) + 0.0002*pwm + 0.1457;
+}
+
+int inverse_cubic(double x){
+  // this function is the result of a cubic function's inverse, obtained from matlab
   
-  if( positive_pwm > 20 && positive_pwm < 70 ) { // acceptance linear range
-    return positive_pwm;
-  }
-  else {
-    if(positive_pwm > 80) {
-      printf("Error: PWM is not in acceptable range./n");
-      return 0;
-    }
+  return (int)(pow(((147573952589676412928*x)/5572392449786181 + pow((pow(((147573952589676412928*x)/5572392449786181 - 632102790618389393758244638159555047229893034175957/47470909521199675931189252779875122596859085800000),2) + 3122879011617526236642070885072818855886306351302285917744713791340273274419820904225939762730255706590632243/7042147658656018892397039849248736387998837701713675676482675488992600832524073443786755125000000000000),(1/2)) - 632102790618389393758244638159555047229893034175957/47470909521199675931189252779875122596859085800000),(1/3)) - 1461678031171954654252356464715037707/(19167628157058046751550999114050000*pow(((147573952589676412928*x)/5572392449786181 + pow((pow(((147573952589676412928*x)/5572392449786181 - 632102790618389393758244638159555047229893034175957/47470909521199675931189252779875122596859085800000),2) + 3122879011617526236642070885072818855886306351302285917744713791340273274419820904225939762730255706590632243/7042147658656018892397039849248736387998837701713675676482675488992600832524073443786755125000000000000),(1/2)) - 632102790618389393758244638159555047229893034175957/47470909521199675931189252779875122596859085800000),(1/3)) + 288282983532959/2476618866571636));
+  
+}
+
+
+void init_lookup() {
+  int i;
+  for (i = 0; i < SIZE; i++) {
+    double force = (i - 100) / 10000;
+    pwm_of_force[i] = inverse_cubic(force);
+    //print lookup
+    printf("%d\n", pwm_of_force[i]);
   }
 }
 
-double pwm_force_neg (double force) {
-  int negative_pwm = pwm_greater_than (force);
+int pwm_force_pos (double force) {
+  int positive_pwm = pwm_greater_than (force);
   
-  if( negative_pwm < -20 && negative_pwm > -70 ) { // acceptance linear range
-    return negative_pwm;
+  if( positive_pwm >= 20 && positive_pwm <= 70 ) { // acceptance linear range
+    return positive_pwm;
   }
   else {
-    if(negative_pwm < -80) {
-      printf("Error: PWM is not in acceptable range./n");
-      return 0;
+    return inverse_cubic(force);
     }
   }
-}    
 
-int main() {
-  init_lookup();
-  char str[256];
-  double force;
-  
-  double front_pwm, back_pwm1, back_pwm2;
-  printf("Enter a force for a pwm to generate\n");
-  
-  scanf("%s", str);
-  force = atof(str);
 
-  printf("Force entered: %f\n", force);
+int pwm_force_neg (double force) {
+  int negative_pwm = pwm_lesser_than (force);
   
-  double pwm_pos = pwm_force_pos(force);
-  double pwm_neg = pwm_force_neg(force);
+  if( negative_pwm <= -20 && negative_pwm >= -70 )  // acceptance linear range
+    return negative_pwm;
   
-  if(pwm_pos == 0 && pwm_neg == 0) { 
-    // given a force, we find the approximate corresponding pwm from the lookups
-    int i;
-    for(i=0; i<NUM_ELEMENTS+1; i++) {
-      if (force == array_of_force[i] ) {
-	front_pwm = array_of_pwm[i];
-	back_pwm1 = front_pwm/2;
-	back_pwm2 = front_pwm/2;
-	break;
-      }
-    }
-    printf("FRONT PWM: %f\n", front_pwm);
-    printf("BACK PWM 1: %f\n",back_pwm1);
-    printf("BACK PWM 2: %f\n",back_pwm2);
-  }
-  else { // motor linearization
-    printf("Positive PWM: %f\n", pwm_pos);
-    printf("Negative PWM: %f\n", pwm_neg);
+  else 
+    return inverse_cubic(force);
     
-    front_pwm = pwm_pos;
+  }
+    
+
+void pwm_force() {
+  
+  init_lookup();
+  
+  while(1){
+    
+    char str[256];
+    double force;
+  
+    int front_pwm, back_pwm1, back_pwm2;
+    printf("Enter a force for a pwm to generate\n");
+  
+    scanf("%s", str);
+    force = atof(str);
+
+    printf("Force entered: %f\n", force);
+  
+    if( force < 0)
+      front_pwm = pwm_force_neg(force);
+    else
+      front_pwm = pwm_force_pos(force);
+    
+    
+    // with the pwm for the front motor, divide that same number by two to distribute 
+    // pwm for the two back motors
     back_pwm1 = front_pwm/2;
     back_pwm2 = front_pwm/2;
-    
-    printf("FRONT PWM: %f\n", front_pwm);
-    printf("BACK PWM 1: %f\n",back_pwm1);
-    printf("BACK PWM 2: %f\n",back_pwm2);
+  
+    printf("FRONT PWM: %d\n", front_pwm);
+    printf("BACK PWM 1: %d\n",back_pwm1);
+    printf("BACK PWM 2: %d\n",back_pwm2);
   }
-  return 0;
 }
--- trunk/vision_new/pwm_force/pwm_force.h
+++ trunk/vision_new/pwm_force/pwm_force.h
@@ -2,6 +2,7 @@
 #define _PWM_FORCE_H_
 
 #include <stdio.h>
+#include <math.h>
 
 #define NUM_ELEMENTS 80
 
@@ -9,64 +10,35 @@
 // this aims to implement motor linearization for the motors of the sub.
 // what this does is given a force to be applied, it would calculate the 
 // pwm necessary to balance the sub (2 back motors each equal 1/2 a front motor)
-// given by the equations interpolated from the data.
+// given by the equations from the data.
 
 // the ranges given by PWM < -20 and PWM > 20 (not after PWM > |70| ) are linear, so 
 // the two linear equations will be used.
 // the rest of the values outside that range will be used by the quartic equation 
 // that most accurately reflects the overall (all) data.
 
-double pwm_force(double force);
-
-// some helper functions:
-double square (double a) {
-  return (a*a);
-}
-
-double cubic (double a) {
-  return (a*a*a);
-}
-
-double quartic (double a) {
-  return (a*a*a*a);
-}
+// note: pwms are ints!!!!!!!!!
+void pwm_force();
 
 // next, are the functions that represent the equations to be used:
 // the linear equations
-double pwm_greater_than(double force) {
-  return ((force + 0.698636363)/0.045);
-}
+int pwm_greater_than(double force);
 
-double pwm_lesser_than(float force) {
-  return (-(force + 0.82990909)/0.0502);
-}
+int pwm_lesser_than(double force);
 
 // the quartic equation is much more complicated, we need a lookup for this. Also
 // do not use factoring polynomials since this makes the programming/math ugly
 
-double force_quartic(double pwm) {
-  return (-5e-8)*quartic(pwm) - (5e-7)*cubic(pwm) + 0.0008*square(pwm) + 0.0002*pwm - 0.0303;
-}
-// we need two lookups: one for storing an array of pwms and a corresponding lookup
-// that stores an array of forces
+double force_cubic(int pwm);
 
-// we need these arrays to be global since it has to be accessed somewhere by FGPA
+int inverse_cubic(double force);
 
-double array_of_pwm[NUM_ELEMENTS+1];
-double array_of_force[NUM_ELEMENTS+1];
+#define SIZE 200
+//int pwm_of_force[SIZE];
 
-// this function initializes the lookups declared from the header file for array of pwms
-// and array of forces
+void init_lookup();
 
-void init_lookup() {
-  int i;
-  for(i=0; i<NUM_ELEMENTS+1; i++) {
-    array_of_pwm[i] = i;
-    array_of_force[i] = force_quartic(array_of_pwm[i]);
-  }
-}
-
-double pwm_force_pos (double force);
-double pwm_force_neg (double force);
+int pwm_force_pos (double force);
+int pwm_force_neg (double force);
 
 #endif
