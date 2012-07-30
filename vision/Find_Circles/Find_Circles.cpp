840d3d3076b4d29efe2671753b3570676e3491bb
vincentlu@hotmail.ca <vincentlu@hotmail.ca@62d9add7-2908-a477-2ee2-5dc9016e4318>
2012-07-30 19:55:46 +0000

    applied vision_BUOY functionality to the Find_Circles function, with a bit of cleaning up

--- vision/Find_Circles/Find_Circles.cpp
+++ vision/Find_Circles/Find_Circles.cpp
@@ -1,43 +1,65 @@
 #include "Find_Circles.h"
 #include <stdio.h>
-using namespace cv;
 
-void Find_Circles(Mat img, vector<Vec3f> results, char* name) {
-  img = imread(name, 1);
-  if( !img.data )
-    { printf("unable to read img\n"); }
-  Mat gray;
-  cvtColor( img, gray, CV_BGR2GRAY );
-  GaussianBlur( gray, gray, Size(9, 9), 2, 2 );
-  /// Make this a black/white image. 
-  Mat bw = gray > 128;
-  HoughCircles( gray, results, CV_HOUGH_GRADIENT, 1, gray.rows/8, 200, 100, 0, 0 );
-  ncircles = (int)results.size();
-  printf("	Number of circles: %d\n", ncircles );
-  /// Loop through circles detected, and draw them
-  for( int i = 0; i < results.size(); i++ )
-  {
-      Point pt(cvRound(results[i][0]), cvRound(results[i][1]));
-      int radius = cvRound(results[i][2]);
-      
-      //circle( img, pt, radius, Scalar(100,50,100), 3, 8, 0 );
-      printf("	Coordinates of circle: (%f, %f)\n", results[i][0], results[i][1]);
-      printf("	Radius of circle: %d\n", radius);
-   }
+
+void Find_Circles(IplImage* img, CvSeq* results, char* name) {
   
-  /// Display image.
-  namedWindow( "Find Circles", CV_WINDOW_AUTOSIZE );
-  imshow( "Find Circles", bw );
+  int buoy_x, buoy_y;
+  float radius;
   
-  waitKey(0);
+  img = cvLoadImage(name);
+  
+  int edge_thresh = 1;
+  IplImage* gray = cvCreateImage (cvGetSize(img), IPL_DEPTH_8U, 1);
+  IplImage* edge = cvCreateImage (cvGetSize(img), IPL_DEPTH_8U, 1);
+  CvMemStorage* storage=cvCreateMemStorage(0);
+  
+  //show original image
+  cvShowImage("original", img);
+  cvWaitKey(0);
+  
+  //convert image colour
+  cvCvtColor(img, gray, CV_BGR2GRAY); 
+  
+  //set the converted image's origin
+  gray->origin=0;
+  
+  //color threshold
+  cvThreshold(gray,gray,100,255,CV_THRESH_BINARY); 
+  
+  //smooth the image to reduce unneccesary results
+  cvSmooth( gray, gray, CV_GAUSSIAN, 11, 11 );
+  
+  //get edges
+  cvCanny(gray, edge, (float)edge_thresh, (float)edge_thresh*3, 5);
+   
+  // show image produced after Canny is applied
+  cvShowImage("edge", edge);
+  cvWaitKey(0);
+  
+  //get circles
+  results = cvHoughCircles(gray, storage, CV_HOUGH_GRADIENT, 1, 100, 50, 40);
+  
+  // output all circles detected
+  int ncircles=results->total; 
+  printf("Number of Circles: %d\n", ncircles);
+  // if any circles are found, draw them as you loop through them.
+  for(int i = 0; i<ncircles; i++ ){
+      float* p = (float*)cvGetSeqElem( results, i );
+      cvCircle( img, cvPoint(cvRound(p[0]),cvRound(p[1])), 3, CV_RGB(255,0,0), -1, 8, 0 );
+      cvCircle( img, cvPoint(cvRound(p[0]),cvRound(p[1])), cvRound(p[2]), CV_RGB(200,0,0), 1, 8, 0 );
+      buoy_x = p[0]; buoy_y = p[1];
+      printf("Centre: %f, %f. Radius: %f\n", p[0], p[1], p[2]);
+  }  
+  cvReleaseMemStorage(&storage);
+  cvReleaseImage (&gray); 
+  cvReleaseImage (&edge);  
 }
 
+
 int main (int argc, char** argv) {
-  
-  Mat img;
-  vector<Vec3f> circles;
-  
-  /// here, to read the circle, you just pass in the image read (Mat type) and the "circles" Vec3f vector (to define coordinates and radius)
+  IplImage* img;
+  CvSeq* circles;
   Find_Circles(img, circles, argv[1]);
   return 0;
 }
