3d416fdd8c3f2c2dbf091afd5d1c13761880edb4
vincentlu@hotmail.ca <vincentlu@hotmail.ca@62d9add7-2908-a477-2ee2-5dc9016e4318>
2012-07-30 19:55:46 +0000

    applied vision_BUOY functionality to the Find_Circles function, with a bit of cleaning up

--- trunk/vision/Find_Circles/Find_Circles.h
+++ trunk/vision/Find_Circles/Find_Circles.h
@@ -23,6 +23,6 @@ using namespace cv;
 int	ncircles; // this is initially garbage. However, it is used to count the number of circles	
 		  // if used outside the function.
 
-void Find_Circles(Mat src, vector<Vec3f> results, char* name);
+void Find_Circles(IplImage* img, CvSeq* results, char* name);
 
 #endif
