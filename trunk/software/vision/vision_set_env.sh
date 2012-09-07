5921febb970446c085a8196ab5009b6d13042173
rzhao01@gmail.com <rzhao01@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2012-09-07 02:39:45 +0000

    Added script that changes env var for settings dir, dont think it is possible to use Makefile to invoke it successfully though

---
+++ trunk/software/vision/vision_set_env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pushd $(dirname $(readlink -f "$BASH_SOURCE")) > /dev/null
+MDA_VISION_SETTINGS_PATH="$PWD/settings"
+popd > /dev/null
+echo $SCRIPT_DIR
+
+export MDA_VISION_SETTINGS_PATH
+echo "MDA_VISION_SETTINGS_PATH=$MDA_VISION_SETTINGS_PATH"
