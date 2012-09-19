5defbc23962255174aa63809a66c2d6590d0bfd2
stetorvs@gmail.com <stetorvs@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2012-09-19 12:21:33 +0000

    Minor updates to template script (comments).

--- trunk/software/scripts/template.c
+++ trunk/software/scripts/template.c
@@ -1,7 +1,7 @@
 #include "scripts.h" /* Must be included */
 
-// Other include files
-#include "unistd.h"
+// Other include files (only system header files should be here)
+#include <unistd.h>
 
 // Define a main function. Any other code you wish to write in functions
 // should be in the same file.
@@ -18,15 +18,16 @@ int main()
 
   /* YOUR CODE GOES HERE */
 
-  sleep(5);
-  int x, y, z, d;
+  sleep(2); // Pause for 2 seconds
+
+  int x, y, z, d; // Get acceleration vector and depth
   get_accel(&x, &y, &z);
   d = get_depth();
 
   printf("Acceleration is: (%d, %d, %d)\n", x, y, z);
   printf("Current depth is: %d\n", d);
 
-  int p = get_power();
+  int p = get_power(); // Check if power is still on
 
   printf("Power system %s\n", p ? "is good": "has failed");
 
