a4184dc66840bb36383c0852d73f73c8f30267d9
rzhao01@gmail.com <rzhao01@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2012-09-29 21:46:20 +0000

    Adds interface code to vision modules. Adds control folder properly.

--- software/vision/lib/mda_vision.h
+++ software/vision/lib/mda_vision.h
@@ -11,15 +11,24 @@
  *      - void calc_vci (int &vci)
  *  
  *  The first function filters the source image into _filtered_img
- *  The second calculates relevant information using the _filtered_img 
+ *  The second calculates relevant information using the _filtered_img
+ *
+ *  Besides this you have the option of implementing get_range() and get_angle()
+ *  You would do this if your module legitimately uses those values.
  */
 /// ########################################################################
 class MDA_VISION_MODULE_BASE {
 protected:
-    IplImage* _resized_;
+    // stores resized image
+    IplImage* _resized_; 
+
+    /// this is the interface data you are passing to control
+    int x, y, range;
+    int angle;
+
     /// you must implement these yourself!
     virtual void primary_filter (const IplImage* src) = 0;
-    virtual void calc_vci (int &vci) = 0;
+    virtual void calc_vci () = 0;
     
 public:
     MDA_VISION_MODULE_BASE () { _resized_ = mvCreateImage_Color(); }
@@ -33,8 +42,21 @@ public:
         _resized_->origin = src->origin;
         
         primary_filter (_resized_);
-        calc_vci (vci);
+        calc_vci ();
     };
+
+    /// accessors for the vision-control interface. Note you only overload
+    /// the values your module is responsible for!
+    int get_x () { return x; }
+    int get_y () { return y; }
+    virtual int get_range() { 
+        printf ("Fatal Error: vision_module - You are accessing invalid variable 'range' !\n");
+        assert (0);
+    }
+    virtual int get_angle() { 
+        printf ("Fatal Error: vision_module - You are accessing invalid variable 'angle' !\n");
+        assert (0);
+    }
 };
 /// ########################################################################
 /// ########################################################################
@@ -58,7 +80,9 @@ public:
     ~MDA_VISION_MODULE_TEST ();
     
     void primary_filter (const IplImage* src);
-    void calc_vci (int &vci);
+    void calc_vci ();
+
+    int get_range() { return range; }
 };
 
 
@@ -72,7 +96,8 @@ public:
     ~MDA_VISION_MODULE_GATE ();
     
     void primary_filter (const IplImage* src);
-    void calc_vci (int &vci);
+    void calc_vci ();
+    int get_range() { return range; }
 };
 
 
@@ -86,7 +111,9 @@ public:
     ~MDA_VISION_MODULE_PATH ();
     
     void primary_filter (const IplImage* src);
-    void calc_vci (int &vci);
+    void calc_vci ();
+    int get_range() { return range; }
+    int get_angle() { return angle; }
 };
 
 /// ########################################################################
@@ -99,7 +126,8 @@ public:
     ~MDA_VISION_MODULE_BUOY ();
     
     void primary_filter (const IplImage* src);
-    void calc_vci (int &vci);
+    void calc_vci ();
+    int get_range() { return range; }
 };
 
 #endif
