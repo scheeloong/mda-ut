3be833247b4bb7b03d22585f123f3fd3856a0e78
rzhao01@gmail.com <rzhao01@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2012-10-05 18:52:03 +0000

    Read yaw, pitch and roll from IMU. Changed ga from get acceleration to get attitude.

--- trunk/fpga/fpga_sw/inc/rs232.h
+++ trunk/fpga/fpga_sw/inc/rs232.h
@@ -3,11 +3,11 @@
 #ifndef _MDA_RS232_H
 #define _MDA_RS232_H
 
+#include "controller.h"
+
 void rs232_init();
 void rs232_shell();
 
-int get_imu_yaw();
-int get_imu_pitch();
-int get_imu_roll();
+void get_imu_orientation(struct orientation *);
 
 #endif
