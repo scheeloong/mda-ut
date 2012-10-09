f815fc89331c3a7d07292792d34aaee7d8bf7b48
rzhao01@gmail.com <rzhao01@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2012-10-09 20:01:33 +0000

    Changing HW_NUM to double since this works and we have no reason to change this to int.

--- fpga/fpga_sw/inc/pid.h
+++ fpga/fpga_sw/inc/pid.h
@@ -3,21 +3,19 @@
 #define _PID_H
 
 #define PID_NUM_OLD_VALUES 3
-typedef double HW_NUM;
-
 typedef struct {
     // everything is here is supposed to be private
     // but for setting up the constants we'll access the members directly
-    HW_NUM P, I, D;
-    HW_NUM Const_P, Const_I, Const_D;
+    double P, I, D;
+    double Const_P, Const_I, Const_D;
     float Alpha; // decay constant for integral term
     
     int num_values; // number of values read
-    HW_NUM old_values[PID_NUM_OLD_VALUES];
+    double old_values[PID_NUM_OLD_VALUES];
 } Controller_PID;
 
 inline void PID_Reset (Controller_PID* PID);
-void PID_Update (Controller_PID* PID, HW_NUM value);
-inline HW_NUM PID_Output (Controller_PID* PID);
+void PID_Update (Controller_PID* PID, double value);
+inline double PID_Output (Controller_PID* PID);
 
 #endif
