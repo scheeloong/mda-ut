3ac6be0aacf2e94581178b50c3a1b31d0b536392
rzhao01@gmail.com <rzhao01@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2012-10-18 03:15:25 +0000

    Added prelim code for adaptive color filters. Also a settings file that I forgot.

--- software/vision/lib/mv.h
+++ software/vision/lib/mv.h
@@ -8,6 +8,8 @@
 #include <cv.h>
 #include "mgui.h"
 
+typedef unsigned char uchar;
+
 /** mvCreateImage is a wrapper for cvCreateImage which drops the need for specifying
  *  depth and nChannels. The default mvCreateImage always returns a greyscale image 
  *  while mvCreateImage_Color always returns a color image;
@@ -102,6 +104,40 @@ class mvHSVFilter {
                  unsigned smin=UNCHANGED, unsigned smax=UNCHANGED, 
                  unsigned vmin=UNCHANGED, unsigned vmax=UNCHANGED
                 );    
+};
+
+
+/** mvAdaptiveBox
+ *  This class represents a learning filter. Values between _min and _max are
+ *  accepted. However, values between _min-_min_bound and _max+_max_bound
+ *  will "stretch" the bounds (_min,_max). Over time the box will grow adapt
+ *  to new data
+ */
+class mvAdaptiveBox {
+    uchar min, max;         // min and max bounds
+    uchar min_50, max_50;   // min and max for 50% of the box
+    uchar min_bound, max_bound;  // threshold for growing the box
+
+    uchar orig_min, orig_max; // min and max when initialized. Stored so box doesnt move too far away.
+    static const uchar adjust_unit = 2;      // how much box bounds grow each time
+    static const uchar adjust_limit = 20;     // how much box bounds are allowed to grow
+
+    uchar box_count;         // how many falls within the box
+    uchar box_50_count;      // how many falls within inner 50% of the box
+    uchar box_min_bound_count, box_max_bound_count;  // how many falls inside the flaps
+
+    public:
+    mvAdaptiveBox (uchar Min, uchar Max, uchar Min_Bound, uchar Max_Bound);
+    int accumulate (uchar Data);
+    void adjust_box ();
+};
+
+class mvAdaptiveFilter {
+    mvAdaptiveBox* box_bg[3];       // one for each channel of background
+    mvAdaptiveBox* box_target[3];
+
+    public:
+    mvAdaptiveFilter (const char* Settings_File);
 };
 
 #endif
