bf5aa1c6082e0cf38e07c2fa9023e25ce86287b5
stetorvs@gmail.com <stetorvs@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2012-10-27 23:45:30 +0000

    Basic joystick implementation for dump image.

--- software/interface/JoystickOperation.cpp
+++ software/interface/JoystickOperation.cpp
@@ -2,5 +2,39 @@
 
 void JoystickOperation::work()
 {
-  // TODO: implement
+  // ncurses stuff
+  initscr();
+  cbreak();
+
+  // regular I/O
+  printf(
+         "Commands: \r\n"
+         "  q - exit simulator\r\n"
+         "  p - save input image screenshots as image_[fwd/dwn].jpg\r\n"
+         "\n");
+  fflush(stdout);
+
+  // Take keyboard commands
+  bool loop = true;
+  while (loop) {
+    char c = get_next_char();
+    switch(c) {
+      case 'q':
+         loop = false;
+         break;
+      case 'p':
+         dump_image();
+         break;
+    }
+  }
+
+  // close ncurses
+  endwin();
+}
+
+void JoystickOperation::dump_image()
+{
+  clrtoeol();
+  printf("\rSaved images");
+  fflush(stdout);
 }
