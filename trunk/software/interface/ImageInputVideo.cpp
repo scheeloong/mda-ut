ebd9a334f2333d1c7a680e55f682a515e552984d
stetorvs@gmail.com <stetorvs@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2012-10-29 00:54:32 +0000

    Add missing file. Delete empty dir.

---
+++ trunk/software/interface/ImageInputVideo.cpp
@@ -0,0 +1,15 @@
+#include "ImageInput.h"
+
+ImageInputVideo::ImageInputVideo()
+{
+}
+
+ImageInputVideo::~ImageInputVideo()
+{
+}
+
+const IplImage *ImageInputVideo::get_image(ImageDirection dir)
+{
+  // TODO: implement
+  return NULL;
+}
