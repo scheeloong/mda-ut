8727241a0e36c40b115c5241ad4d07286176d5f1
stetorvs@gmail.com <stetorvs@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2012-10-30 13:52:03 +0000

    Add interface for actuator in terms: setting an attitude change, an absolute attitude or a special command (ie sim or sub specific).

--- trunk/software/interface/ActuatorOutputSubmarine.cpp
+++ trunk/software/interface/ActuatorOutputSubmarine.cpp
@@ -1,5 +1,6 @@
 #include "ActuatorOutput.h"
 #include "SubmarineSingleton.h"
+#include "../scripts/scripts.h"
 
 ActuatorOutputSubmarine::ActuatorOutputSubmarine()
 {
@@ -10,4 +11,29 @@ ActuatorOutputSubmarine::ActuatorOutputSubmarine()
 ActuatorOutputSubmarine::~ActuatorOutputSubmarine()
 {
   SubmarineSingleton::get_instance().decrement_instances();
+}
+
+void ActuatorOutputSubmarine::set_attitude_change(ATTITUDE_CHANGE_DIRECTION dir, int delta)
+{
+  // TODO: implement
+}
+
+void ActuatorOutputSubmarine::set_attitude_absolute(ATTITUDE_DIRECTION dir, int val)
+{
+  // TODO: implement
+}
+
+void ActuatorOutputSubmarine::special_cmd(SPECIAL_COMMAND cmd)
+{
+  // TODO: implement
+  switch(cmd) {
+    case (SUB_POWER_ON):
+      power_on();
+      break;
+    case (SUB_POWER_OFF):
+      power_off();
+      break;
+    default:
+     break;
+  }
 }
