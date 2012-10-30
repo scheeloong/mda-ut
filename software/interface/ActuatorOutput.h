fea94167a0f977cc2186c1f0fe2cf20e420a3e74
stetorvs@gmail.com <stetorvs@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2012-10-30 13:52:03 +0000

    Add interface for actuator in terms: setting an attitude change, an absolute attitude or a special command (ie sim or sub specific).

--- software/interface/ActuatorOutput.h
+++ software/interface/ActuatorOutput.h
@@ -7,18 +7,57 @@
 #ifndef ACTUATOR_OUTPUT_H
 #define ACTUATOR_OUTPUT_H
 
+enum ATTITUDE_CHANGE_DIRECTION {
+  FORWARD,
+  REVERSE,
+  LEFT,
+  RIGHT,
+  SINK,
+  RISE
+};
+
+enum ATTITUDE_DIRECTION {
+  SPEED,
+  YAW,
+  DEPTH
+};
+
+enum SPECIAL_COMMAND {
+  SUB_POWER_ON,
+  SUB_POWER_OFF,
+  SIM_RESET_POS,
+  SIM_RESET_ALL
+};
+
+#define DEFAULT_ATTITUDE_CHANGE 15
+
 /* Actuator Output interface */
 class ActuatorOutput {
   public:
     virtual ~ActuatorOutput() {}
 
     // some methods to actuate the output
+    virtual void set_attitude_change(ATTITUDE_CHANGE_DIRECTION, int) = 0;
+    virtual void set_attitude_absolute(ATTITUDE_DIRECTION, int) = 0;
+    virtual void special_cmd(SPECIAL_COMMAND) = 0;
+
+    virtual void set_attitude_change(ATTITUDE_CHANGE_DIRECTION dir) {set_attitude_change(dir, DEFAULT_ATTITUDE_CHANGE);}
+    virtual void stop()
+    {
+      set_attitude_change(FORWARD, 0);
+      set_attitude_change(LEFT, 0);
+      set_attitude_change(SINK, 0);
+    }
 };
 
 /* A don't care implementation */
 class ActuatorOutputNull : public ActuatorOutput {
   public:
     virtual ~ActuatorOutputNull() {}
+
+    virtual void set_attitude_change(ATTITUDE_CHANGE_DIRECTION dir, int delta) {}
+    virtual void set_attitude_absolute(ATTITUDE_DIRECTION dir, int val) {}
+    virtual void special_cmd(SPECIAL_COMMAND cmd) {}
 };
 
 /* Simulator implementation */
@@ -26,6 +65,10 @@ class ActuatorOutputSimulator : public ActuatorOutput {
   public:
     ActuatorOutputSimulator();
     virtual ~ActuatorOutputSimulator();
+
+    virtual void set_attitude_change(ATTITUDE_CHANGE_DIRECTION, int);
+    virtual void set_attitude_absolute(ATTITUDE_DIRECTION, int);
+    virtual void special_cmd(SPECIAL_COMMAND);
 };
 
 /* The real submarine implementation */
@@ -33,6 +76,10 @@ class ActuatorOutputSubmarine : public ActuatorOutput {
   public:
     ActuatorOutputSubmarine();
     virtual ~ActuatorOutputSubmarine();
+
+    virtual void set_attitude_change(ATTITUDE_CHANGE_DIRECTION, int);
+    virtual void set_attitude_absolute(ATTITUDE_DIRECTION, int);
+    virtual void special_cmd(SPECIAL_COMMAND);
 };
 
 #endif
--- software/interface/ActuatorOutputSimulator.cpp
+++ software/interface/ActuatorOutputSimulator.cpp
@@ -11,3 +11,22 @@ ActuatorOutputSimulator::~ActuatorOutputSimulator()
 {
   SimulatorSingleton::get_instance().decrement_instances();
 }
+
+void ActuatorOutputSimulator::set_attitude_change(ATTITUDE_CHANGE_DIRECTION dir, int delta)
+{
+  // TODO: implement
+}
+
+void ActuatorOutputSimulator::set_attitude_absolute(ATTITUDE_DIRECTION dir, int val)
+{
+  // TODO: implement
+}
+
+void ActuatorOutputSimulator::special_cmd(SPECIAL_COMMAND cmd)
+{
+  // TODO: implement
+  switch(cmd) {
+    default:
+     break;
+  }
+}
