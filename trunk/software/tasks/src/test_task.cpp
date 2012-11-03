1bd1be93176d8c23e99a4e41d1829c231ba475f3
rzhao01@gmail.com <rzhao01@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2012-11-03 05:18:48 +0000

    Forgot a file

---
+++ trunk/software/tasks/src/test_task.cpp
@@ -0,0 +1,27 @@
+#include "mda_tasks.h"
+#include "mda_vision.h"
+
+MDA_TASK_TEST:: MDA_TASK_TEST (AttitudeInput* a, ImageInput* i, ActuatorOutput* o) :
+    MDA_TASK_BASE (a, i, o),
+    window ("task_test")
+{
+    vision_module = new MDA_VISION_MODULE_TEST ();
+}
+
+MDA_TASK_TEST:: ~MDA_TASK_TEST ()
+{
+    delete vision_module;
+}
+
+MDA_TASK_RETURN_CODE MDA_TASK_TEST:: run_task() {
+    while (1) {
+        const IplImage* frame = image_input->get_image(FWD_IMG);
+        window.showImage (frame);
+
+        char c = cvWaitKey (10);
+        if (c == 'q')
+            break;
+    }
+
+    return TASK_DONE;
+}
