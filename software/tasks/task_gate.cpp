644bae6a8cd1dd64f92abaaaf6f7e5a9c52ac24d
rzhao01@gmail.com <rzhao01@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2012-11-11 23:12:49 +0000

    Made the aquatux GL and CV window size read from the common size in common_settings.csv

--- software/tasks/task_gate.cpp
+++ software/tasks/task_gate.cpp
@@ -39,21 +39,33 @@ MDA_TASK_RETURN_CODE MDA_TASK_GATE:: run_task() {
             else if (vision_code == NO_TARGET || vision_code == UNKNOWN_TARGET) {
                 actuator_output->set_attitude_change(FORWARD,5);
             }
-            else if (vision_code == ONE_SEGMENT || vision_code == FULL_DETECT) {
+            else if (vision_code == ONE_SEGMENT) {
                 int x = gate_vision.get_pixel_x();
 
-                if (x < -frame->width/5)
+                if (x < -frame->width/10)
                     actuator_output->set_attitude_change(LEFT,5);
-                else if (x > frame->width/5)
+                else if (x > frame->width/10)
                     actuator_output->set_attitude_change(RIGHT,5);
                 else 
                     actuator_output->set_attitude_change(FORWARD,5);
 
+            } 
+            else if (vision_code == FULL_DETECT) {
                 // if we can see full gate and range is less than 400 we are done the gate part
-                if (vision_code == FULL_DETECT && gate_vision.get_range() < 400) {
+                if (gate_vision.get_range() < 400) {
                     actuator_output->set_attitude_change(FORWARD,5);
                     done_gate = true;
+                    continue;
                 }
+
+                int x = gate_vision.get_pixel_x();
+
+                if (x < -frame->width/6)
+                    actuator_output->set_attitude_change(LEFT,5);
+                else if (x > frame->width/6)
+                    actuator_output->set_attitude_change(RIGHT,5);
+                else 
+                    actuator_output->set_attitude_change(FORWARD,5);
             }
         }
         else {
@@ -66,7 +78,7 @@ MDA_TASK_RETURN_CODE MDA_TASK_GATE:: run_task() {
 
             MDA_VISION_RETURN_CODE vision_code = path_vision.filter(down_frame);
 
-            if (vision_code == ONE_SEGMENT || vision_code == FULL_DETECT) {
+            if (vision_code == ONE_SEGMENT || vision_code == FULL_DETECT || vision_code == UNKNOWN_TARGET) {
                 ret_code = TASK_DONE;
                 actuator_output->stop();
                 break;
