34c74957f34a894da771d5618a7960e7dda78966
rzhao01@gmail.com <rzhao01@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2012-11-12 02:19:30 +0000

    Added files needed for the 4 tasks we have vision modules for. Each task calls its vision module and can be selected in Aquatux.

---
+++ trunk/software/tasks/task_buoy.cpp
@@ -0,0 +1,44 @@
+#include "mda_tasks.h"
+#include "mda_vision.h"
+
+MDA_TASK_BUOY:: MDA_TASK_BUOY (AttitudeInput* a, ImageInput* i, ActuatorOutput* o) :
+    MDA_TASK_BASE (a, i, o)
+{
+}
+
+MDA_TASK_BUOY:: ~MDA_TASK_BUOY ()
+{
+}
+
+MDA_TASK_RETURN_CODE MDA_TASK_BUOY:: run_task() {
+    puts("Press q to quit");
+
+    MDA_VISION_MODULE_BUOY buoy_vision;
+    MDA_TASK_RETURN_CODE ret_code = TASK_MISSING;
+
+    while (1) {
+        const IplImage* frame = image_input->get_image(DWN_IMG);
+        if (!frame) {
+            ret_code = TASK_ERROR;
+            break;
+        }
+
+        MDA_VISION_RETURN_CODE vision_code = buoy_vision.filter(frame);
+        (void) vision_code;
+
+        // Ensure debug messages are printed
+        fflush(stdout);
+        // Exit if instructed to
+        char c = cvWaitKey(3);
+        if (c != -1) {
+            CharacterStreamSingleton::get_instance().write_char(c);
+        }
+        if (CharacterStreamSingleton::get_instance().wait_key(1) == 'q'){
+            actuator_output->stop();
+            ret_code = TASK_QUIT;
+            break;
+        }
+    }
+
+    return ret_code;
+}
