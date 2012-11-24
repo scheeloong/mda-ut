0ce56768823f10bd7ec34e3536ea859daf295371
kai.vanes@gmail.com <kai.vanes@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2012-11-24 20:04:24 +0000

    Added vision and control code for fram task

--- trunk/software/tasks/task_frame.cpp
+++ trunk/software/tasks/task_frame.cpp
@@ -16,15 +16,57 @@ MDA_TASK_RETURN_CODE MDA_TASK_FRAME:: run_task() {
     MDA_VISION_MODULE_FRAME frame_vision;
     MDA_TASK_RETURN_CODE ret_code = TASK_MISSING;
 
+    bool done_frame = false;
+    ATTITUDE_CHANGE_DIRECTION dir;
+
     while (1) {
         const IplImage* frame = image_input->get_image(FWD_IMG);
         if (!frame) {
             ret_code = TASK_ERROR;
             break;
         }
+        if(!done_frame){
+            MDA_VISION_RETURN_CODE vision_code = frame_vision.filter(frame);
+            if (vision_code == FATAL_ERROR) {
+                ret_code = TASK_ERROR;
+                break;
+            }
+            else if (vision_code == NO_TARGET || vision_code == UNKNOWN_TARGET) {
+                actuator_output->set_attitude_change(FORWARD);
+            }
+            else if (vision_code == ONE_SEGMENT) {
+                int ang_x = frame_vision.get_angular_x();
+                //int ang_y = frame_vision.get_angular_y();
 
-        MDA_VISION_RETURN_CODE vision_code = frame_vision.filter(frame);
-        (void) vision_code;
+                //if(ang_y < 5.0) actuator_output->set_attitude_change(SINK);
+                //else if(ang_y > 5.0) actuator_output->set_attitude_change(RISE);
+                if(abs(ang_x) > 5.0) actuator_output->set_attitude_change(RIGHT, ang_x);
+                //else if(ang_x < -5.0) actuator_output->set_attitude_change(LEFT, ang_x);
+                else actuator_output->set_attitude_change(FORWARD);
+            } 
+            else if (vision_code == FULL_DETECT) {
+                // if we can see full frame and range is less than 400 we are done the frame part
+                if (frame_vision.get_range() < 400) {
+                    // set target yaw to current yaw and go forward
+                    actuator_output->stop();
+                    done_frame = true;
+                    ret_code = TASK_DONE;
+                    continue;
+                }
+
+                int ang_x = frame_vision.get_angular_x();
+                //int ang_y = frame_vision.get_angular_y();
+
+                //if(ang_y < 5.0) actuator_output->set_attitude_change(SINK);
+                //else if(ang_y > 5.0) actuator_output->set_attitude_change(RISE);
+                if(abs(ang_x) > 5.0) actuator_output->set_attitude_change(RIGHT, ang_x);
+                //else if(ang_x < -5.0) actuator_output->set_attitude_change(LEFT, ang_x);
+                else actuator_output->set_attitude_change(FORWARD);
+            }
+
+            //(void) vision_code;
+
+        }
 
         // Ensure debug messages are printed
         fflush(stdout);
--- trunk/software/tasks/vision_frame.cpp
+++ trunk/software/tasks/vision_frame.cpp
@@ -7,6 +7,9 @@
     #define DEBUG_PRINT(format, ...)
 #endif
 
+#define MAX(a,b) (a<b)?b:a
+#define MIN(a,b) (a>b)?b:a
+
 const char MDA_VISION_MODULE_FRAME::MDA_VISION_FRAME_SETTINGS[] = "vision_frame_settings.csv";
 
 /// #########################################################################
@@ -43,12 +46,15 @@ void MDA_VISION_MODULE_FRAME:: primary_filter (const IplImage* src) {
 MDA_VISION_RETURN_CODE MDA_VISION_MODULE_FRAME:: calc_vci () {
     MDA_VISION_RETURN_CODE retval = FATAL_ERROR;
     unsigned nClusters = _KMeans.nClusters();
+    int imWidth  =  (int)_filtered_img->width*0.5;
+    int imHeight =  (int)_filtered_img->height*0.5;
 
     if(nClusters == 0) {
         printf ("Frame: No clusters =(\n");
         return NO_TARGET;
     }
-    if(nClusters == 1){
+    else if(nClusters == 1){
+        DEBUG_PRINT("Frame: 1 Cluster...\n");
         int x1 = _KMeans[0][0].x, x2 = _KMeans[0][1].x;
         int y1 = _KMeans[0][0].y, y2 = _KMeans[0][1].y;    
 
@@ -56,17 +62,15 @@ MDA_VISION_RETURN_CODE MDA_VISION_MODULE_FRAME:: calc_vci () {
         double slope = ((double)y1-y2)/denom;
 
         if (abs(slope) < 0.2) { // single horizontal line
-            m_pixel_x = (x1-x2)/2;
-            m_pixel_y = (y1-y2)/2;
+            m_pixel_x = (int)(x1+x2)*0.5 - imWidth;
+            m_pixel_y = (int)(y1-y2)*0.5 - imHeight;
             m_range = ((float)(FRAME_REAL_WIDTH) * _filtered_img->width) / ((x2-x1) * TAN_FOV_X);
-            printf("Frame Range: %d\n", m_range);
-
             retval = ONE_SEGMENT;
             goto RETURN_CENTROID;
         }
         else if (abs(slope) > 6) { // single vertical line
-            m_pixel_x = (x1-x2)/2;
-            m_pixel_y = (y1-y2)/2;
+            m_pixel_x = (int)(x1+x2)*0.5 - imWidth;
+            m_pixel_y = (int)(y1+y2)*0.5 - imHeight;
 
             retval = ONE_SEGMENT;
             goto RETURN_CENTROID;
@@ -76,6 +80,117 @@ MDA_VISION_RETURN_CODE MDA_VISION_MODULE_FRAME:: calc_vci () {
             goto RETURN_CENTROID;
         }
     }
+    else if(nClusters == 2){
+        DEBUG_PRINT ("Frame: 2 clusters...\n");
+        int x00 = _KMeans[0][0].x,   y00 = _KMeans[0][0].y;
+        int x01 = _KMeans[0][1].x,   y01 = _KMeans[0][1].y;
+        int x10 = _KMeans[1][0].x,   y10 = _KMeans[1][0].y;
+        int x11 = _KMeans[1][1].x,   y11 = _KMeans[1][1].y;
+
+        //float height0 = abs(y01 - y00); // height of first 1ine
+        //float height1 = abs(y11 - y10); // height of second line
+        float height = -1;
+        float width  = -1;
+
+        float denom0 = (x00==x01) ? 1 : abs(x00-x01);
+        double slope0 = ((double)y00-y01)/denom0;
+
+        float denom1 = (x10==x11) ? 1 : abs(x10-x11);
+        double slope1 = ((double)y10-y11)/denom1;
+
+        if(abs(slope0) < 0.2 && abs(slope1) < 0.2){
+            printf("Frame Sanity Failure: 2 Horizontal Lines?????\n");
+            m_pixel_x = (int)(x00+x01+x10+x11)*0.25 - imWidth;
+            m_pixel_y = (int)(y00+y01+y10+y11)*0.25 - imHeight;
+            width = (int)(abs(x00-x01) + abs(x10-x11))*0.25;
+            retval = UNKNOWN_TARGET;
+        }
+        else if(abs(slope0) < 0.2 && abs(slope1) > 6){
+            //printf("1 of Each\n");
+            m_pixel_x = (int)(x00 + x01)*0.5 - imWidth;
+            m_pixel_y = (int)(MAX(y10,y11)+y00+y01)*0.333 - imHeight;
+            width  = (int)abs(x00-x01);
+            height = (int)abs(y10-y11);
+            retval = ONE_SEGMENT;  //Need other retval here
+        }
+        else if(abs(slope0) > 6 && abs(slope1) < 0.2){
+            //printf("1 of Each\n");
+            m_pixel_x = (int)(x10 + x11)*0.5 - imWidth;
+            m_pixel_y = (int)(MAX(y00,y01)+y10+y11)*0.333 - imHeight;
+            width  = (int)abs(x10-x11);
+            height = (int)abs(y00-y01);
+            retval = ONE_SEGMENT;
+        }
+        else if(abs(slope0) > 6 && abs(slope1) > 6){
+            //printf("2 Vertical Lines\n");
+            m_pixel_x = (int)(x00+x01+x10+x11)*0.25 - imWidth;
+            m_pixel_y = (int)(y00+y01+y10+y11)*0.25 - imHeight;
+            width  = (int)(abs(x00-x11) + abs(x10-x11))*0.5;
+            height = (int)(abs(y00-y11) + abs(y10-y11))*0.5;
+            retval = ONE_SEGMENT;
+        }
+        else{
+            DEBUG_PRINT("Frame Sanity Failure: Lines Incorrectly Oriented\n");  
+            retval = UNKNOWN_TARGET;   
+            goto RETURN_CENTROID;   
+        }
+
+        m_range = (float)(FRAME_REAL_WIDTH * _filtered_img->width / width * TAN_FOV_X);
+    }
+
+    else if(nClusters == 3){
+        DEBUG_PRINT ("Frame: 3 clusters =)\n");
+        int x00 = _KMeans[0][0].x,   y00 = _KMeans[0][0].y;
+        int x01 = _KMeans[0][1].x,   y01 = _KMeans[0][1].y;
+        int x10 = _KMeans[1][0].x,   y10 = _KMeans[1][0].y;
+        int x11 = _KMeans[1][1].x,   y11 = _KMeans[1][1].y;
+        int x20 = _KMeans[2][0].x,   y20 = _KMeans[2][0].y;
+        int x21 = _KMeans[2][1].x,   y21 = _KMeans[2][1].y;
+
+        float height = -1;
+        float width  = -1;
+
+        float denom0 = (x00==x01) ? 1 : abs(x00-x01);
+        double slope0 = ((double)y00-y01)/denom0;
+
+        float denom1 = (x10==x11) ? 1 : abs(x10-x11);
+        double slope1 = ((double)y10-y11)/denom1;
+
+        float denom2 = (x20==x21) ? 1 : abs(x20-x21);
+        double slope2 = ((double)y20-y21)/denom2;
+
+        if(abs(slope0) < 0.2 && abs(slope1) > 6 && abs(slope2) > 6){
+            m_pixel_x = (int)(x00+x01+x10+x11+x20+x21)*0.167 - imWidth;
+            m_pixel_y = (int)(MIN(y10,y11)+MIN(y20,y21)+y00+y01)*0.25 - imHeight;
+            width  = (int)(abs(x00-x01)+abs((x10+x11)-(x20+x21)))*0.333;
+            height = (int)(-1*y00-y01+MAX(y10,y11)+MAX(y20,y21))*0.5;
+            retval = FULL_DETECT;
+        }
+        else if(abs(slope0) > 6 && abs(slope1) < 0.2 && abs(slope2) > 6){
+            m_pixel_x = (int)(x00+x01+x10+x11+x20+x21)*0.167 - imWidth;
+            m_pixel_y = (int)(MAX(y00,y01)+MAX(y20,y21)+y10+y11)*0.25 - imHeight;
+            width  = (int)(abs(x10-x11)+abs((x00+x01)-(x20+x21)))*0.333;
+            height = (int)(-1*y10-y11+MAX(y00,y01)+MAX(y20,y21))*0.5;
+            retval = FULL_DETECT;
+        }
+        else if(abs(slope0) > 6 && abs(slope1) > 6 && abs(slope2) < 0.2){
+            m_pixel_x = (int)(x00+x01+x10+x11+x20+x21)*0.167 - imWidth;
+            m_pixel_y = (int)(MAX(y10,y11)+MAX(y00,y01)+y20+y21)*0.25 - imHeight;
+            width  = (int)(abs(x20-x21)+abs((x10+x11)-(x00+x01)))*0.333;
+            height = (int)(-1*y20-y21+MAX(y10,y11)+MAX(y00,y01))*0.5;
+            retval = FULL_DETECT;
+        }
+        else{
+            DEBUG_PRINT("Frame Sanity Failure: Incorrect line arrangement\n");
+            m_pixel_x = (int)(x00+x01+x10+x11+x20+x21)*0.167 -imWidth;
+            m_pixel_y = (int)(y00+y01+y10+y11+y20+y21)*0.167 - imHeight;
+            retval = UNKNOWN_TARGET;
+            goto RETURN_CENTROID;
+        }
+
+        m_range = (float)(FRAME_REAL_WIDTH * _filtered_img->width / width * TAN_FOV_X);
+    }
+
     else{
         printf("nClusters = %d unhandled\n", nClusters);
         return NO_TARGET;
