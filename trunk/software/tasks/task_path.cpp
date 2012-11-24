5e02e557c92156db968b9dd71f0184097bf585fb
walkerm930@gmail.com <walkerm930@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2012-11-24 20:05:27 +0000

    Implemented task path

--- trunk/software/tasks/task_path.cpp
+++ trunk/software/tasks/task_path.cpp
@@ -13,8 +13,11 @@ MDA_TASK_PATH:: ~MDA_TASK_PATH ()
 MDA_TASK_RETURN_CODE MDA_TASK_PATH:: run_task() {
     puts("Press q to quit");
 
+
     MDA_VISION_MODULE_PATH path_vision;
     MDA_TASK_RETURN_CODE ret_code = TASK_MISSING;
+
+    bool done_path = false;
 
     while (1) {
         const IplImage* frame = image_input->get_image(DWN_IMG);
@@ -22,9 +25,51 @@ MDA_TASK_RETURN_CODE MDA_TASK_PATH:: run_task() {
             ret_code = TASK_ERROR;
             break;
         }
+        if (!done_path){
+            MDA_VISION_RETURN_CODE vision_code = path_vision.filter(frame);
+            
+            if (vision_code == NO_TARGET){
+                actuator_output->set_attitude_change(FORWARD);           
+            }
+            else if (vision_code == UNKNOWN_TARGET){
+                int ang_x = path_vision.get_angular_x();
 
-        MDA_VISION_RETURN_CODE vision_code = path_vision.filter(frame);
-        (void) vision_code;
+                if (abs(ang_x) < 5){
+                    actuator_output->set_attitude_change(FORWARD);           
+                }else{
+                    actuator_output->set_attitude_change(RIGHT, ang_x);
+                }
+            }
+            else if (vision_code == ONE_SEGMENT || vision_code == FULL_DETECT) {
+                int ang_x = path_vision.get_angular_x();
+                int pix_x = path_vision.get_pixel_x();
+                int pix_y = path_vision.get_pixel_y();
+                int distance = sqrt(pow(pix_y,2) + pow(pix_x,2));
+
+                printf("distance = %d\n",distance );
+
+                if (distance < frame->height/6){
+                    actuator_output->set_attitude_absolute(DEPTH,475);
+                    // actuator_output->set_attitude_change(SINK,20);
+
+                    if(attitude_input->depth() > 450){
+                        int pos_ang = path_vision.get_angle();
+
+                        actuator_output->set_attitude_change(RIGHT,pos_ang);
+                        if(abs(pos_ang) < 4){
+                            done_path = true;
+                            break;
+                        }
+                    }
+                }else{
+                    if (abs(ang_x) < 5){
+                        actuator_output->set_attitude_change(FORWARD,1);           
+                    }else{
+                        actuator_output->set_attitude_change(RIGHT, ang_x);
+                    }
+                }
+            }
+        }
 
         // Ensure debug messages are printed
         fflush(stdout);
@@ -38,6 +83,12 @@ MDA_TASK_RETURN_CODE MDA_TASK_PATH:: run_task() {
             ret_code = TASK_QUIT;
             break;
         }
+    }
+
+    if(done_path){
+        ret_code = TASK_DONE;
+    }else{
+        ret_code = TASK_REDO;
     }
 
     return ret_code;
