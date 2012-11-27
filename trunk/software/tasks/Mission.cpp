a06a4186c2499d92e8ec8422bfe25bb8bd6bc71d
stetorvs@gmail.com <stetorvs@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2012-11-27 01:23:30 +0000

    Skip the buoy task and add it to the Mission. The mission can now reach the frame task, after skipping the buoy task.

--- trunk/software/tasks/Mission.cpp
+++ trunk/software/tasks/Mission.cpp
@@ -9,17 +9,24 @@ void Mission::work()
 {
   // Tasks
   MDA_TASK_GATE gate(attitude_input, image_input, actuator_output);
+  MDA_TASK_BUOY buoy(attitude_input, image_input, actuator_output);
   MDA_TASK_PATH path(attitude_input, image_input, actuator_output);
 
   // List of tasks to be performed in order (NULL-terminated)
-  MDA_TASK_BASE *tasks[] = {&gate, &path, NULL};
+  MDA_TASK_BASE *tasks[] = {&gate, &path, &buoy, &path, NULL};
 
   // Pointer to current task
   MDA_TASK_BASE **task_ptr = tasks;
 
+  // Result of a task
+  MDA_TASK_RETURN_CODE ret_code;
+
   // Run each task until the list of tasks is complete
   while (*task_ptr) {
-    (*task_ptr)->run_task();
+    ret_code = (*task_ptr)->run_task();
+    if (ret_code == TASK_QUIT) {
+      break;
+    }
     task_ptr++;
   }
 }
