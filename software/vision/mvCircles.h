0994a84c1b82149c5dae3bbc2ac716838edf8ed9
rzhao01@gmail.com <rzhao01@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2012-12-05 02:34:58 +0000

    Fixes bugs in mvBinaryGradient. Minor change to mvAdvancedCircles

--- software/vision/mvCircles.h
+++ software/vision/mvCircles.h
@@ -11,18 +11,18 @@
 
 #define CIRCLE_THICKNESS 2
 
-/** CIRCLE_STRUCT - simple structure to represent a circle */
+/** MV_CIRCLE - simple structure to represent a circle */
 // note OpenCV stores its circle finding algorithm's output in a
 // float* array, where [0],[1] are x,y and [2] is radius
-// so you can easily typecast the float* into CIRCLE_STRUCT*
+// so you can easily typecast the float* into MV_CIRCLE*
 // and have easier, more readable code
 typedef struct _Circle_ {
     int x;
     int y;
     float rad;
-} CIRCLE_STRUCT;
+} MV_CIRCLE;
 
-typedef std::pair<CIRCLE_STRUCT, unsigned> M_CIRCLE;
+typedef std::pair<MV_CIRCLE, unsigned> CIRCLE_U_PAIR;
 typedef std::pair<float,float> FLOAT_PAIR;
 
 class mvAdvancedCircles {
@@ -39,20 +39,20 @@ class mvAdvancedCircles {
     unsigned grid_width, grid_height;
 
     std::vector<FLOAT_PAIR> cos_sin_vector;     // list of cos/sin values, precalculated
-    std::vector<M_CIRCLE> accepted_circles;    // list of circles found
+    std::vector<CIRCLE_U_PAIR> accepted_circles;    // list of circles found
 
     PROFILE_BIN bin_findcircles;
 
     private:
-    int get_circle_from_3_points (CvPoint p1, CvPoint p2, CvPoint p3, CIRCLE_STRUCT &Circle);
-    int check_circle_validity (IplImage* img, CIRCLE_STRUCT Circle);
+    int get_circle_from_3_points (CvPoint p1, CvPoint p2, CvPoint p3, MV_CIRCLE &Circle);
+    int check_circle_validity (IplImage* img, MV_CIRCLE Circle);
 
     public:
     mvAdvancedCircles (const char* settings_file);
     ~mvAdvancedCircles ();
     void findCircles (IplImage* img);
     int ncircles ();
-    CIRCLE_STRUCT operator [] (unsigned index) { return accepted_circles[index].first; }
+    MV_CIRCLE operator [] (unsigned index) { return accepted_circles[index].first; }
     void drawOntoImage (IplImage* img);
 };
 
