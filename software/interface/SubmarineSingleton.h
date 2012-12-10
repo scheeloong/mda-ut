3fe86372f7b6c2db4262ac410538596211f9207d
stetorvs@gmail.com <stetorvs@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2012-12-10 21:02:01 +0000

    Finish commenting the header files in software/interface/

--- software/interface/SubmarineSingleton.h
+++ software/interface/SubmarineSingleton.h
@@ -1,3 +1,11 @@
+/* SubmarineSingleton
+
+  This class implements the communication interface to the submarine, via its FPGA. This communication is done through the nios2-terminal,
+  and the nuances of communicating through an interactive shell is encapsulated by this singleton. The FPGA is responsible for gathering
+  attitude data, for relaying motor control commands to the submarine motors, and power management. This singleton allows a high-level
+  program to interface with the FPGA.
+*/
+
 #ifndef SUBMARINE_SINGLETON_H
 #define SUBMARINE_SINGLETON_H
 
