93d787e918d71216f0a593efce091b76749ea418
stetorvs@gmail.com <stetorvs@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2012-12-13 21:19:10 +0000

    Remove convergence problem when path sanity failure checks fail (ie infinite loop bouncing left and right). Still return the average angle of the two lines, since it's our *best* guess at this point.

--- trunk/software/tasks/task_path.cpp
+++ trunk/software/tasks/task_path.cpp
@@ -29,21 +29,20 @@ MDA_TASK_RETURN_CODE MDA_TASK_PATH:: run_task() {
         image_input->get_image();
         if (!done_path){
             MDA_VISION_RETURN_CODE vision_code = path_vision.filter(frame);
+            int pos_ang = path_vision.get_angle();
             
             if (vision_code == NO_TARGET){
                 actuator_output->set_attitude_change(FORWARD);           
             }
             else if (vision_code == UNKNOWN_TARGET){
-                int ang_x = path_vision.get_angular_x();
 
-                if (abs(ang_x) < 5){
+                if (abs(pos_ang) < 5){
                     actuator_output->set_attitude_change(FORWARD);           
                 }else{
-                    actuator_output->set_attitude_change(LEFT, ang_x);
+                    actuator_output->set_attitude_change(LEFT, pos_ang);
                 }
             }
             else if (vision_code == ONE_SEGMENT || vision_code == FULL_DETECT) {
-                int ang_x = path_vision.get_angular_x();
                 int pix_x = path_vision.get_pixel_x();
                 int pix_y = path_vision.get_pixel_y();
                 int distance = sqrt(pow(pix_y,2) + pow(pix_x,2));
@@ -54,8 +53,6 @@ MDA_TASK_RETURN_CODE MDA_TASK_PATH:: run_task() {
                     actuator_output->set_attitude_absolute(DEPTH, 585);
 
                     if(attitude_input->depth() > 570){
-                        int pos_ang = path_vision.get_angle();
-
                         actuator_output->set_attitude_change(LEFT,pos_ang);
                         if(abs(pos_ang) < 5){
                             done_path = true;
@@ -63,10 +60,10 @@ MDA_TASK_RETURN_CODE MDA_TASK_PATH:: run_task() {
                         }
                     }
                 }else{
-                    if (abs(ang_x) < 5){
+                    if (abs(pos_ang) < 5){
                         actuator_output->set_attitude_change(FORWARD);
                     }else{
-                        actuator_output->set_attitude_change(LEFT, ang_x);
+                        actuator_output->set_attitude_change(LEFT, pos_ang);
                     }
                 }
             }
