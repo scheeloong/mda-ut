fe28639cb9475a03d0da40985a5024599aa4f1a1
rzhao01@gmail.com <rzhao01@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2013-02-04 04:41:52 +0000

    Changed PID to only have 1 DOF. Added valid_oldValues. Changed computation to occur at PID_Output. PID constants dont seem to matter currently since movement always hits YAW_MAX.

--- trunk/software/sim/PID.cpp
+++ trunk/software/sim/PID.cpp
@@ -1,34 +1,32 @@
 #include "PID.h"
 #include <stdio.h>
+#include <algorithm> // for std::min
+
+// wrapper to make <sys/time> gettimeofday easier to use
+unsigned long long gettimeofday () {
+    struct timeval T;
+    gettimeofday (&T, NULL);
+    return (T.tv_sec*1000000 + T.tv_usec);
+}
 
 PID::PID(){
 	Kp =  Ki =  Kd =  alpha = 0;
+	P = I = D = 0;
+    valid_oldValues = 0;
+    unsigned long long t_us = gettimeofday();
 	
-	gettimeofday(&t,NULL);
-	unsigned long long t_us = t.tv_sec*1000000 + t.tv_usec;
-	
-	for(int i=0; i<PID_NUM_OLD_VALUES; i++){
+	for(int i=0; i<PID_NUM_OLD_VALUES; i++)
 		times[i] = (t_us-i);	//initialize to now, with an offset such that 1/(t[i]-t[i+1]) != inf
-		for(int j=0; j<PID_DEG_FREEDOM; j++){
-			oldValues[j][i] = 0;
-		}
-	}
-	for(int k=0; k<PID_DEG_FREEDOM; k++) P[k] = D[k] = I[k] = 0;
 }
 
 PID::PID(float _Kp, float _Ki, float _Kd, float _alpha){
 	Kp = _Kp; Ki = _Ki; Kd = _Kd; alpha = _alpha;
-
-	gettimeofday(&t,NULL);
-	unsigned long long t_us = t.tv_sec*1000000 + t.tv_usec;
+	P = I = D = 0;
+    valid_oldValues = 0;
+	unsigned long long t_us = gettimeofday();
 	
-	for(int i=0; i<PID_NUM_OLD_VALUES; i++){
+	for(int i=0; i<PID_NUM_OLD_VALUES; i++)
 		times[i] = (t_us-i);	//initialize to now, with an offset such that 1/(t[i]-t[i+1]) != inf
-		for(int j=0; j<PID_DEG_FREEDOM; j++){
-			oldValues[j][i] = 0;
-		}
-	}
-	for(int k=0; k<PID_DEG_FREEDOM; k++) P[k] = D[k] = I[k] = 0;
 }
 
 
@@ -44,59 +42,60 @@ void PID::setAlpha(float _alpha){
 
 void PID::PID_Reset(){
 /*Resets controller while preserving PID constants*/
+	P = I = D = 0;
+    valid_oldValues = 0;
 
-	gettimeofday(&t,NULL);
-	unsigned long long t_us = t.tv_sec*1000000 + t.tv_usec;
+	unsigned long long t_us = gettimeofday();
 
 	for(int i=0; i<PID_NUM_OLD_VALUES; i++){
 		times[i] = (t_us-i);	//initialize to now, with an offset such that 1/(t[i]-t[i+1]) != inf
-		for(int j=0; j<PID_DEG_FREEDOM; j++){
-			oldValues[j][i] = 0;
-		}
 	}
-	for(int k=0; k<PID_DEG_FREEDOM; k++) P[k] = D[k] = I[k] = 0;
 }
 
-void PID::PID_Update(float *values){	
+void PID::PID_Update(float value){	
 /*Updates PID signals internally for each controlled value
   Expects that values is of size PID_DEG_FREEDOM*/
 
-	for(int j=PID_NUM_OLD_VALUES-1; j>0; j--){
-		for (int i=0; i<PID_DEG_FREEDOM; i++) oldValues[i][j] = oldValues[i][j-1];
+	for(int j=PID_NUM_OLD_VALUES-1; j>0; j--){ // shift every time value back one slot
 		times[j] = times[j-1];
+        oldValues[j] = oldValues[j-1];
 	}
-	gettimeofday(&t,NULL);
-	unsigned long long t_us = t.tv_sec*1000000 + t.tv_usec;
 
-	times[0] = t_us;
+	times[0] = gettimeofday();
+    oldValues[0] = value;
 
-	for(int i=0; i<PID_DEG_FREEDOM; i++){
-		oldValues[i][0] = values[i];
-
-		I[i] = I[i] * (1-alpha) + ((times[0] - times[1])*(oldValues[i][0] + oldValues[i][1])/2) / 1000000;  //Trapezoidal integration
-		if (times[0] - times[1] == 0) {
-			D[i] = 0;
-		} else {
-			D[i] = (oldValues[i][0] - oldValues[i][1])/(times[0]-times[1]) * 1000000;  //Backwards difference differentiation
-		}
-		P[i] = values[i];
-	}
+    if (valid_oldValues < PID_NUM_OLD_VALUES)
+        valid_oldValues++;
 }
 
-float* PID::PID_Output(){
+float PID::PID_Output(){
 /*Output control signals for each controlled vlaue
   Output is of length PID_DEG_FREEDOM*/
+    float P=0, I=0, D=0;
+   
+    P = oldValues[0];
 
-	float* output = new float[PID_DEG_FREEDOM];
-	for (int i=0; i<PID_DEG_FREEDOM; i++){
-		output[i] = Kp*P[i] + Kd*D[i] + Ki*I[i];
-	}
-	return (output);
+    // use trapezoid rule.
+    for (int i = 0; i < valid_oldValues-1; i++) {
+        I = I*(1-alpha) + (times[i] - times[i+1]) * (oldValues[i] + oldValues[i+1]);
+    }
+    I /= 2.0; // divide by 2 due to trapezoid rule 
+    I /= 1000000.0;
+
+    for (int i = 0; i < std::min(PID_NUM_DIFF_VALUES,valid_oldValues-1); i++) {
+        unsigned long long delta_t = times[i] - times[i+1];
+        delta_t = (delta_t > 0) ? delta_t : 1;
+        D += (oldValues[i] - oldValues[i+1]) / delta_t;
+    }
+    D /= PID_NUM_DIFF_VALUES;
+    D *= 1000000.0;
+    
+	return (Kp*P + Ki*I + Kd*D);
 }
 
 void PID::debug(){
-	printf("P: %f I: %f D: %f\n", P[0], I[0], D[0]);
-	printf(" times: ");
+	printf(" num_valid: %d\n", valid_oldValues);
+    printf(" times: ");
 	for(int j=0; j<PID_NUM_OLD_VALUES; j++){
 		printf("%lld ",times[j]);
 	}
--- trunk/software/sim/PID.h
+++ trunk/software/sim/PID.h
@@ -3,23 +3,23 @@
 
 #include <sys/time.h>
 
-#define PID_NUM_OLD_VALUES 3
-#define PID_DEG_FREEDOM 2		//Number of properties being controlled: 0=>y, 1=>yaw
-
 class PID {
+    static const int PID_NUM_OLD_VALUES = 5;
+    static const int PID_NUM_DIFF_VALUES = 2; // num of old values to use during differentiation
+
 	protected:
-		float P[PID_DEG_FREEDOM], I[PID_DEG_FREEDOM], D[PID_DEG_FREEDOM];
+		float P, I, D;                          //PID values 
 		float Kp, Ki, Kd;						//PID constants
 
-		struct timeval t;								//Record real world time of execution for numerical integration/differentiation
 		unsigned long long times[PID_NUM_OLD_VALUES];
 
-		float oldValues[PID_DEG_FREEDOM][PID_NUM_OLD_VALUES];	//store old values
+        int valid_oldValues;
+		float oldValues[PID_NUM_OLD_VALUES];                	//store old values
 		float alpha;											//decay constant for I
 
 	public:
 		PID();
-		PID(float _Kp, float _Ki, float _Kd, float _alpha);
+		PID(float _Kp, float _Ki, float _Kd, float _alpha=0);
 		~PID();
 
 		void setK(float _Kp, float _Ki, float _Kd);
@@ -27,8 +27,8 @@ class PID {
 
 		void PID_Reset();
 
-		void   PID_Update(float *values);	//performs direct update on the model values, based on the target
-		float *PID_Output();
+		void PID_Update(float value);    	
+		float PID_Output();
 
 		void debug();
 };
