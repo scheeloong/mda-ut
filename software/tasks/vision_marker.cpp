24f941afac348eb8d0147a1e985162399cab5106
ye.alice.ay@gmail.com <ye.alice.ay@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2013-03-23 19:07:46 +0000

    Added multiple targets for vision_marker

--- software/tasks/vision_marker.cpp
+++ software/tasks/vision_marker.cpp
@@ -43,25 +43,33 @@ void MDA_VISION_MODULE_MARKER:: primary_filter (IplImage* src) {
 }
 
 MDA_VISION_RETURN_CODE MDA_VISION_MODULE_MARKER:: calc_vci () {
-    MDA_VISION_RETURN_CODE retval = FATAL_ERROR;
-    double target[7] = {0.256755, 0.001068, 0.006488, 0.001148, 0.000003, 0.000037, -0.000000};//the plane
-
+    MDA_VISION_RETURN_CODE retval = NO_TARGET;
+    double target[2][7] = {
+                           {0.256755, 0.001068, 0.006488, 0.001148, 0.000003, 0.000037, -0.000000}, //the plane
+                           {0.256755, 0.001068, 0.006488, 0.001148, 0.000003, 0.000037, -0.000000}
+                          };
+    
     double hu[7];
     mvHuMoments(_filtered_img, hu);
 
     double dist = 0;
-    printf("{");
-    for (int i = 0; i < 7; ++i)
-    {
-        printf("%lf, ", hu[i]);
-        dist += fabs(hu[i] - target[i]);
-    }
-    printf("\b\b}: %lf\n",dist);
 
-    if (dist < HU_THRESH){
-        retval = FULL_DETECT;
+    for (int j = 0; j < 2; ++j)
+    {
+        printf("{");
+        for (int i = 0; i < 7; ++i)
+        {
+            printf("%lf, ", hu[i]);
+            dist += fabs(hu[i] - target[j][i]);
+        }
+        printf("\b\b}: %lf\n",dist);
+    
+        if (dist < HU_THRESH){
+            retval = FULL_DETECT;
+            printf("Match found!! :)\n");
+        }
+        dist = 0;
     }
-    else retval = NO_TARGET;
 
     return retval;
 }
