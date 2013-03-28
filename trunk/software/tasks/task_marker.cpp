88d24d2068a6e725633138da0383a3eac610ef23
stetorvs@gmail.com <stetorvs@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2013-03-28 22:47:24 +0000

    Add very simple marker task control code that goes forward and calls marker vision. The task completes after it finds 2 valid targets. No left/right alignment is done.

--- trunk/software/tasks/task_marker.cpp
+++ trunk/software/tasks/task_marker.cpp
@@ -17,32 +17,52 @@ MDA_TASK_RETURN_CODE MDA_TASK_MARKER:: run_task() {
     MDA_TASK_RETURN_CODE ret_code = TASK_MISSING;
     bool done_marker = false;
 
-    while (1) {
-        IplImage* frame = image_input->get_image();
+    int markers_dropped = 0;
+    static const int starting_depth = 630;
+
+    // this is rough depth of the marker dropper targets
+    actuator_output->set_attitude_absolute(DEPTH, starting_depth);
+    sleep(2);
+
+    while (!done_marker) {
+        IplImage* frame = image_input->get_image(DWN_IMG);
         if (!frame) {
             ret_code = TASK_ERROR;
             break;
         }
         MDA_VISION_RETURN_CODE vision_code = marker_vision.filter(frame);
+
+        // clear fwd image
+        int fwd_frame_ready = image_input->ready_image();
+        (void) fwd_frame_ready;
         
-        if (!done_marker) {
-            if (vision_code == FATAL_ERROR) {
-                ret_code = TASK_ERROR;
-                break;
-            }
-            else if (vision_code == NO_TARGET || vision_code == UNKNOWN_TARGET) {
-                actuator_output->set_attitude_change(FORWARD);
-            }
-            else if (vision_code == FULL_DETECT) {
+        if (vision_code == FATAL_ERROR) {
+            ret_code = TASK_ERROR;
+            break;
+        }
+        else if (vision_code == NO_TARGET || vision_code == UNKNOWN_TARGET) {
+            actuator_output->set_attitude_change(FORWARD);
+        }
+        else if (vision_code == FULL_DETECT) {
+            markers_dropped++;
+            printf("Dropping marker here.\n");
+            fflush(stdout);
+            actuator_output->stop();
+            sleep(2);
+
+            if (markers_dropped == 2) {
                 ret_code = TASK_DONE;
-                actuator_output->stop();
-                printf ("Plane Found.\n");
+                printf ("Done marker task.\n");
                 break;
             }
-            else {
-                printf ("Error: %s: line %d\ntask module recieved an unhandled vision code.\n", __FILE__, __LINE__);
-                exit(1);
-            }
+
+            // continue going forward to search for more targets
+            actuator_output->set_attitude_change(FORWARD);
+            sleep(1);
+        }
+        else {
+            printf ("Error: %s: line %d\ntask module recieved an unhandled vision code.\n", __FILE__, __LINE__);
+            exit(1);
         }
        
         // Ensure debug messages are printed
