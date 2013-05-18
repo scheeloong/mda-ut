d25225a1f51eab83d5a4bd846b60950412e402f3
rzhao01@gmail.com <rzhao01@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2013-05-18 07:20:06 +0000

    Adds contour matching based on loaded images

--- software/vision/mvContours.h
+++ software/vision/mvContours.h
@@ -5,11 +5,22 @@
 #include "mv.h"
 #include "mgui.h"
 
+typedef std::vector<double> HU_MOMENTS;
+static const int CONTOURS_MATCH_NORMAL = 0;
+static const int CONTOURS_MATCH_RECIP = 1;
+static const int CONTOURS_MATCH_FRACTION = 2;
+
 class mvContours {
+    // list of images that contain objects to match against
+    static const int NUM_CONTOUR_IMAGES = 10;
+    static const char* contour_rect_images[];
+    std::vector<HU_MOMENTS> hu_moments_vector;
+
     CvMemStorage* m_storage;
     CvSeq* m_contours;
 
 private:
+    // approximates a rounded contour with a polygon
     CvSeq* approx_poly (CvSeq* contour_to_approx, int accuracy) { 
         return cvApproxPoly(
                 contour_to_approx,
@@ -20,14 +31,20 @@ private:
                 0                       // contour level. 0 = first only, 1 = all contours level with and below the first
             ); 
     }
+
     void get_ellipse_parameters (IplImage* img, CvSeq* contour1, CvPoint &centroid, float &angle);
-    float match_contour (CvSeq* contour1, CvSeq* contour2);
+
+    void get_hu_moments (CvSeq* contour1, HU_MOMENTS& hu_moments);
+    
+    // match the hu moments of the contour against all the ones in hu_moments_vector
+    void match_contour_with_database (CvSeq* contour1, int &best_match_index, double &best_match_diff, int method);
+    
     void draw_contours (CvSeq* contours_to_draw, IplImage* img) {
         cvDrawContours (
                     img,
                     contours_to_draw,
                     cvScalar(200,200,200),  // contour color
-                    cvScalar(0,0,0),        // background color
+                    cvScalar(100,100,100),        // background color
                     0                       // max contours level
             );
     }
@@ -36,7 +53,7 @@ public:
     mvContours ();
     ~mvContours ();
 
-    bool find_rectangle (IplImage* img, CvPoint &centroid, float &angle);
+    bool find_rectangle (IplImage* img, CvPoint &centroid, float &angle, int method=CONTOURS_MATCH_RECIP);
 
     void drawOntoImage (IplImage* img) { draw_contours (m_contours, img); }        
 
