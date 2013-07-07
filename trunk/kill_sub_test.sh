30f5e75a70dba9c4f05e0c57f8dbfb42b8250b09
stetorvs@gmail.com <stetorvs@gmail.com@62d9add7-2908-a477-2ee2-5dc9016e4318>
2013-07-07 14:08:22 +0000

    Add kill script if aquatux executable hangs.

---
+++ trunk/kill_sub_test.sh
@@ -0,0 +1,7 @@
+# Kill nicely
+pkill -2 aquatux
+#Force kill
+pkill -9 aquatux
+pkill -9 nios2-terminal
+pkill -9 nios2-terminal-wrapped
+echo "You should now be able to rerun the sub test without problems"
